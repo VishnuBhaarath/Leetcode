93c66aacbead9b3ebb99dcd1d779228ecfe133e6
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-01-11 10:56:48 +0530

    Time: 455 ms (22.08%), Space: 93.5 MB (28.84%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -4,29 +4,26 @@ public:
         int n=matrix.size();
         for(int i=0;i<n;i++){
             map<int,int> umap;
+              map<int,int> umap1;
             for(int j=0;j<n;j++){
                 if(matrix[i][j]<1 || matrix[i][j]>n){
+                    return false;
+                }
+                 if(matrix[j][i]<1 || matrix[j][i]>n){
                     return false;
                 }
                 umap[matrix[i][j]]+=1;
                 if(umap[matrix[i][j]]>1){
                     return false;
                 }
+                umap1[matrix[j][i]]+=1;
+                if(umap1[matrix[j][i]]>1){
+                    return false;
+                }
             }
         }
         
-         for(int i=0;i<n;i++){
-            map<int,int> umap;
-            for(int j=0;j<n;j++){
-                if(matrix[j][i]<1 || matrix[j][i]>n){
-                    return false;
-                }
-                umap[matrix[j][i]]+=1;
-                if(umap[matrix[j][i]]>1){
-                    return false;
-                }
-            }
-        }
+         
         return true;
     }
 };
