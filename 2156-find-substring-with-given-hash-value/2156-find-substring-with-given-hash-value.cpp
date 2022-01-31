38cc5440b6a11ea13777b3f6002a67333ea4163d
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-01-31 13:19:00 +0530

    Time: 25 ms (52.94%), Space: 10.3 MB (11.76%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.cpp
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.cpp
@@ -5,7 +5,7 @@ public:
         for (int i = n - 1; i >= 0; --i) {
             cur = (cur * p + s[i] - 'a' + 1) % m;
             if (i + k >= n)
-                pk = pk * p % m;
+                pk = (pk * p) % m;
             else
                 cur = (cur - ((s[i + k] - 'a' + 1) * pk )% m + m) % m;
             if (cur == hashValue)
