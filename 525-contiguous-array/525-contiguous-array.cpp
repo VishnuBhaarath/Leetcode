e23fc7aafac7211b61943b842fe5661a2c286d1a
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-02-04 11:22:19 +0530

    Time: 148 ms (58.31%), Space: 91.8 MB (5.04%) - LeetHub

---
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        vector<int> presum;
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                sum-=1;
+            }
+            else{
+                sum+=1;
+            }
+            presum.push_back(sum);
+        }
+        
+        map<int,int> umap;
+        for(int i=n-1;i>=0;i--){
+            umap[presum[i]]=i+1;
+        }
+        int ans=0;
+        for(int i=n-1;i>=0;i--){
+            if(presum[i]==0){
+                ans=max(ans,i+1);
+            }
+            else{
+                int idx=umap[presum[i]];
+                if(idx!=0){
+                    ans=max(ans,i+1-idx);
+                }
+            }
+        }
+        return ans;
+    }
+};
