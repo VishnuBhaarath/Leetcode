d4612650494a8c33ebe303997562714d6d270ac9
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-02-08 11:09:42 +0530

    Time: 5 ms (17.94%), Space: 5.9 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int n=num;
-        if((n/10)==0){
-            return n;
-        }
-        int sum=0;
-        while(n){
-            int val=0;
-            while(n){
-                int r=n%10;
-                n=n/10;
-                val+=r;
-            }
-            n=val;
-            
-            sum+=val;
-            if((n/10)==0){
-                return n;
-            }
-        }
-        cout<<sum;
-        return 0;
+       if(num==0){
+           return 0;
+       }
+       if((num%9)==0){
+           return 9;
+       } 
+        return num%9;
     }
 };
