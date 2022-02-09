4a60081b6738ce02b8449f88e13adf829b81c478
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-02-09 11:01:29 +0530

    Time: 94 ms (9.54%), Space: 17 MB (5.12%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,20 +1,45 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        map<pair<int,int>,int> umap;
-        int cnt=0;
+        map<int,int> umap;
         for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if((abs(nums[i]-nums[j]))==k){
-                    
-                    pair<int,int> p={nums[i],nums[j]};
-                    if(nums[j]<nums[i]){
-                        p={nums[j],nums[i]};
+            umap[nums[i]]+=1;
+        }
+      
+        map<pair<int,int>,int> umap1;
+        int cnt=0;
+        if(k==0){
+            for(auto x:umap){
+                if(x.second>1){
+                    cnt+=1;
+                }
+            }
+            return cnt;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(umap[nums[i]-k]!=0){
+                int a=nums[i];
+                int b=nums[i]-k;
+                pair<int,int> p={a,b};
+                if(b<a){
+                    p={b,a};
+                }
+                if(umap1[p]==0){
+                    umap1[p]+=1;
+                    cnt+=1;
+                }
+            }
+            else{
+                if(umap[nums[i]+k]!=0){
+                    int a=nums[i];
+                    int b=nums[i]+k;
+                    pair<int,int> p={a,b};
+                    if(b<a){
+                        p={b,a};
                     }
-                    if(umap[p]==0){
-                        
+                    if(umap1[p]==0){
+                        umap1[p]+=1;
                         cnt+=1;
-                        umap[p]+=1;
                     }
                 }
             }
