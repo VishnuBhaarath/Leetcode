753dc06f8f400cc9693a8f38ff3240c79afa4744
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-02-17 11:14:24 +0530

    Time: 51 ms (36.52%), Space: 40.7 MB (24.44%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    map<vector<int>,int> umap;
     vector<vector<int>> combine(int n, int k) {
-        vector<int> v(n,0);
-        vector<int> v1;
-        backtrack(0,0,n,k,v1);
-        return ans;
+    vector<int>num;
+        for(int i=1; i<=n; i++){
+            num.push_back(i);
+        }
+        
+        vector<vector<int>>result;
+        vector<int>answer;
+        form_combinations(num,0,answer,result,k);
+        return result;
     }
-    void backtrack(int i, int cnt,int n,int k,vector<int> v1){
-        if((n-i+cnt)<k){
-           
+   void form_combinations( vector<int>num, int i,vector<int>&answer, vector<vector<int>>&result, int k){
+        if(k==0){
+            result.push_back(answer);
             return;
         }
-        if(cnt>k){
-          
-            return;
-        }
-        if(cnt==k){
-           
-                ans.push_back(v1);
-          
-            return;
-        }
-        if(i==n){
-            return;
-        }
-       
-        v1.push_back(i+1);
         
-        backtrack(i+1,cnt+1,n,k,v1);
-       
-        v1.pop_back();
+        if(i==num.size()){
+            return;
+        }
         
-        backtrack(i+1,cnt,n,k,v1);
+        if(num.size()-i < k){ // TLE solved using this
+            return;
+        }
+        
+        answer.push_back(num[i]);
+        form_combinations(num,i+1,answer,result,k-1);
+        answer.pop_back();
+        form_combinations(num,i+1,answer,result,k);
+        return;
     }
 };
