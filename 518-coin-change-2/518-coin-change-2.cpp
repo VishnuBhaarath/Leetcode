b22c2683a134849ccfe94510d13dc88d08ce0d51
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-05-30 17:55:52 +0530

    Time: 29 ms (53.34%), Space: 7.2 MB (87.47%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        sort(coins.begin(),coins.end());
+        vector<int> dp(amount+1,0);
+        if(amount==0){
+            return 1;
+        }
+         for(int i=0;i<coins.size();i++){
+             for(int j=0;j<dp.size();j++){
+                 if(j+coins[i]>=dp.size()){
+                     break;
+                 }
+                 if(j==0){
+                     dp[coins[i]]+=1;
+                 }
+                 else{
+                     dp[coins[i]+j]=dp[coins[i]+j]+dp[j];
+                 }
+             }
+         }
+        cout<<"\n";
+        return dp[amount];
+    }
+};
