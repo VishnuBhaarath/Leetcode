b96db4bf7c680838f716349d4327319c1d819118
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-05-31 00:43:13 +0530

    Added solution - LeetHub

---
+++ Minimum cost to fill given weight in a bag - GFG/minimum-cost-to-fill-given-weight-in-a-bag.cpp
@@ -0,0 +1,102 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+#include <bits/stdc++.h>
+using namespace std;
+//Position this line where user code will be pasted.
+
+class Solution{
+		
+
+	public:
+	int minimumCost(int cost[], int N, int W) 
+	{ 
+        // Your code goes here
+        vector<int> v(W+1,0);
+        vector<vector<int>> dp(N+1,v);
+        int ans=-1;
+        for(int i=1;i<dp.size();i++){
+          
+            for(int j=1;j<dp[i].size();j++){
+                if(j<i){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else if(i==j){
+                   if(cost[i-1]!=-1){
+                       if(dp[i-1][j]!=0){
+                         dp[i][j]=min(dp[i-1][j],cost[i-1]);}
+                       else{
+                         dp[i][j]=cost[i-1];
+                         }
+                     }
+                    else{
+                        dp[i][j]=dp[i-1][j];
+                    }
+                }
+                else{
+                  if(cost[i-1]==-1){
+                      dp[i][j]=dp[i-1][j];
+                  }
+                  else{
+                    if(dp[i-1][j]==0){
+                        if(dp[i][j-i]!=0)
+                        {
+                        dp[i][j]=dp[i][j-i]+cost[i-1];
+                            
+                        }
+                    }
+                    else{
+                        if(dp[i][j-i]!=0){
+                        dp[i][j]=min(dp[i-1][j],dp[i][j-i]+cost[i-1]);}
+                        else{
+                            dp[i][j]=dp[i-1][j];
+                        }
+                    }
+                }
+             }
+                if(j==W){
+                    if(dp[i][j]!=0){
+                        if(ans==-1){
+                            ans=dp[i][j];
+                        }
+                        else{
+                            ans=min(ans,dp[i][j]);
+                        }
+                    }
+                }
+            }
+          
+        }
+       
+        return ans;
+	} 
+};
+
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, w;
+        cin >> n >> w;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.minimumCost(a, n, w) << "\n";
+	     
+    }
+    return 0;
+}  // } Driver Code Ends
