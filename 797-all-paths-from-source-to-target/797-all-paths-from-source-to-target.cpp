516614f883946fe960ad5bcdc5e7361e1ff69d5d
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-06-14 11:28:52 +0530

    Time: 177 ms (5.01%), Space: 51.7 MB (5.05%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void backtrack(vector<vector<int>>& graph, int node, int dest, vector<int> v)
-    {
-        if(node == dest){
-            ans.push_back(v);
-        return;}
-        
-        
-        for(int i=0; i<graph[node].size(); i++)
-        {
-            v.push_back(graph[node][i]);
-            backtrack(graph, graph[node][i], dest, v);
-            v.pop_back();
-        }
-    }
+      vector<vector<int>> ans;
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-         int dest = graph.size() - 1;
+      
         vector<int> v;
         v.push_back(0);
-        backtrack(graph, 0, dest, v);
+        int dest=graph.size()-1;
+        traversal(graph,v,0,dest);
         return ans;
+    }
+    void traversal(vector<vector<int>> graph,vector<int> v,int i,int dest){
+        if(i==dest){
+            ans.push_back(v);
+            return;
+        }
+        for(int j=0;j<graph[i].size();j++){
+            v.push_back(graph[i][j]);
+            traversal(graph,v,graph[i][j],dest);
+            v.pop_back();
+        }
+        return;
     }
 };
