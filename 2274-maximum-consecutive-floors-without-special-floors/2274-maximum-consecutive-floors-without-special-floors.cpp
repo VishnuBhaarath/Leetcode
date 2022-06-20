6bdde7546413c48dfcd8d63bcbdc2f84f3dd3fc3
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-06-20 11:50:54 +0530

    Time: 227 ms (57.37%), Space: 57.9 MB (81.10%) - LeetHub

---
+++ 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxConsecutive(int bottom, int top, vector<int>& special) {
+        sort(special.begin(),special.end());
+        int ans=special[0]-bottom;
+        for(int i=1;i<special.size();i++){
+            int val=special[i]-special[i-1]-1;
+            ans=max(ans,val);
+        }
+        ans=max(ans,top-special[special.size()-1]);
+        return ans;
+    }
+};
