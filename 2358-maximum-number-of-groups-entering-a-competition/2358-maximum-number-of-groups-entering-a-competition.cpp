27a57cbaa08a74bf89051d9c5e0902a8450374d2
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-07-31 17:54:52 +0530

    Time: 235 ms (100.00%), Space: 67.1 MB (100.00%) - LeetHub

---
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximumGroups(vector<int>& grades) {
+        vector<long long int> presum;
+        presum.push_back(grades[0]);
+        for(int i=1;i<grades.size();i++){
+            presum.push_back(presum[presum.size()-1]+grades[i]);
+        }
+        int i=0;
+        int cnt=1;
+        int j=2;
+        int prev=presum[i];
+        while(j<presum.size()){
+            int curr=presum[j]-presum[i];
+            cnt+=1;
+            int k=j-i;
+            cout<<j;
+            cout<<" ";
+            cout<<i;
+            cout<<" ";
+            cout<<cnt;
+            cout<<"\n";
+            if((j+k+1)>=presum.size()){
+                return cnt;
+            }
+            int temp=j;
+            i=j;
+            j=temp+k+1;
+            
+        }
+        cout<<"\n";
+        return cnt;
+    }
+};
