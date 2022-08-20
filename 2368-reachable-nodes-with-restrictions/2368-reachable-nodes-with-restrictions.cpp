731093069cba2bcc692d0fb98dc00a7be44dbc1a
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2022-08-20 11:44:58 +0530

    Time: 2376 ms (5.01%), Space: 217.8 MB (6.81%) - LeetHub

---
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        map<int,int> umap;
+        map<int,vector<int>> umap1;
+        for(int i=0;i<edges.size();i++){
+            umap1[edges[i][0]].push_back(edges[i][1]);
+            umap1[edges[i][1]].push_back(edges[i][0]);
+        }
+        for(int i=0;i<restricted.size();i++){
+            umap[restricted[i]]+=1;
+        }
+        map<int,int> umap2;
+        int cnt=1;
+    
+           
+                queue<int> q;
+                q.push(0);
+                umap2[0]+=1;
+                while(!q.empty()){
+                    vector<int> v=umap1[q.front()];
+                    q.pop();
+                    for(int j=0;j<v.size();j++){
+                        if(umap[v[j]]==0 && umap2[v[j]]==0){
+                            q.push(v[j]);
+                            umap2[v[j]]+=1;
+                            cnt+=1;
+                        }
+                    }
+                }
+            
+            return cnt;
+        
+        return 4;
+    }
+};
