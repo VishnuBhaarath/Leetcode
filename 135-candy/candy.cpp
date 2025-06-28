b5a16558a05ead3dfa6258905928a1c766aab673
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-06-28 12:45:29 +0530

    Time: 207 ms (5.03%) | Memory: 23 MB (26.26%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -17,11 +17,7 @@ public:
                 v[i]=v[i-1]+1;
             }
         }
-        for(int i=0;i<v.size();i++){
-            cout<<v[i];
-            cout<<" ";
-        }
-        cout<<"\n";
+       
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
                 if(v[i]<=v[i+1]){
