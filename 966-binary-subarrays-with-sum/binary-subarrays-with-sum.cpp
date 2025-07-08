e94012a023529817fe776ef83f005a9a317b8fb0
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-07-08 19:13:58 +0530

    Time: 2 ms (57.45%) | Memory: 34.9 MB (34.05%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
+    int func1(vector<int> nums, int goal){
+        int i=0;
+        int j=0;
+        int sum=0;
+        int cnt=0;
+        while(j<nums.size()){
+           sum+=nums[j];
+           if(sum<=goal){
+             cnt+=(j-i)+1;
+           }
+           else{
+            while(sum>goal){
+                if(i<nums.size()){
+                sum-=nums[i];
+                i+=1;}
+                else{
+                    return cnt;
+                }
+            }
+            cnt+=(j-i)+1;
+           }
+           j+=1;
+        }
+        return cnt;
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-      map<int,int> psum;
-      psum[0]+=1;
-      int sum=0;
-      int cnt=0;
-      for(int i=0;i<nums.size();i++){
-        sum+=nums[i];
-        if(sum==goal){
-             cnt+=psum[0];
-        }
-        else if(sum>goal){
-            cnt+=psum[sum-goal];
-        }
-        psum[sum]+=1;
+        int sum1=func1(nums,goal);
+        int sum2=0;
+        if(goal>0){
+        sum2=func1(nums,goal-1);}
         
-      }
-  
-      return cnt;
+        return sum1-sum2;
+
     }
 };
