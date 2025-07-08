f813e0408dbc375629838179cbd664eadd18340e
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-07-08 20:12:28 +0530

    Time: 1 ms (37.99%) | Memory: 22.8 MB (66.11%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        int cnt=0;
         int n=ratings.size();
         vector<int> v (n,1);
         
         for(int i=1;i<ratings.size();i++){
-         
-            if(ratings[i]<=ratings[i-1]){
-                v[i]=1;
-                if(ratings[i]<ratings[i-1]){
-                if(v[i-1]==1){
-                    v[i-1]=2;
-                }}
-            }
-            else{
+            if(ratings[i]>ratings[i-1]){
+               if(v[i]<=v[i-1]){
                 v[i]=v[i-1]+1;
+                
+               }
             }
         }
-       
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                if(v[i]<=v[i+1]){
-                    v[i]=v[i+1]+1;
+        for(int i=n-1;i>0;i--){
+            if(ratings[i-1]>ratings[i]){
+                if(v[i-1]<=v[i]){
+                    v[i-1]=v[i]+1;
+                 
                 }
             }
-        
         }
-
-        int sum=0;
         for(int i=0;i<v.size();i++){
-         
-            sum+=v[i];
+            cnt+=v[i];
         }
-   
-        return sum;
+        cout<<"\n";
+        return cnt;
     }
 };
