a6a39c7e386e18aa2f2257a58910336bd8f19d95
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-07-30 12:52:44 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (44.70%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -6,30 +6,22 @@ public:
         int end=newInterval[1];
         int n=intervals.size();
         int t=0;
-        for(int i=0;i<intervals.size();i++){
-            if(intervals[i][1]<start){
-                dp.push_back({intervals[i][0],intervals[i][1]});
-              
-            }
-            else if(intervals[i][0]>end){
-                if(t==0){
-                dp.push_back({start,end});}
-                t=1;
-                dp.push_back({intervals[i][0],intervals[i][1]});
-            }
-            else {
-                start=min(start,intervals[i][0]);
-                end=max(end,intervals[i][1]);
-                if(i==n-1){
-                    dp.push_back({start,end});
-                    t=1;
-                }
-            }
+        int i=0;
+        while(i<intervals.size() && intervals[i][1]<start){
+            dp.push_back({intervals[i][0],intervals[i][1]});
+            i+=1;
+        }
+        while(i<intervals.size() && intervals[i][0]<=end){
+            start=min(start,intervals[i][0]);
+            end=max(end,intervals[i][1]);
+            i+=1;
+        }
+        dp.push_back({start,end});
 
-        }
-        if(t==0){
-            dp.push_back({start,end});
-        }
+         while(i<intervals.size() && intervals[i][0]>end){
+            dp.push_back({intervals[i][0],intervals[i][1]});
+            i+=1;
+         }
         return dp;
     }
 };
