9703029b9f251b2dff9f8c7776a9ad48e304471c
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-07-30 11:25:10 +0530

    Time: 4 ms (19.20%) | Memory: 21.7 MB (74.23%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {  
-         vector<vector<int>> dp;
-         int i=0;
-         int n=intervals.size();
-         while(i<n && intervals[i][1]<newInterval[0]){
-            dp.push_back({intervals[i][0],intervals[i][1]});
-            i+=1;
-         }
-         while(i<n && intervals[i][0]<=newInterval[1]){
-            newInterval[0]=min(newInterval[0],intervals[i][0]);
-            newInterval[1]=max(newInterval[1],intervals[i][1]);
-            i+=1;
-         }
-         dp.push_back({newInterval[0],newInterval[1]});
-         while(i<n && intervals[i][0]>newInterval[1]){
-            dp.push_back({intervals[i][0],intervals[i][1]});
-            i+=1;
-         }
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> dp;
+        int start=newInterval[0];
+        int end=newInterval[1];
+        int n=intervals.size();
+        int t=0;
+        for(int i=0;i<intervals.size();i++){
+            if(intervals[i][1]<start){
+                dp.push_back({intervals[i][0],intervals[i][1]});
+              
+            }
+            else if(intervals[i][0]>end){
+                if(t==0){
+                dp.push_back({start,end});}
+                t=1;
+                dp.push_back({intervals[i][0],intervals[i][1]});
+            }
+            else {
+                start=min(start,intervals[i][0]);
+                end=max(end,intervals[i][1]);
+                if(i==n-1){
+                    dp.push_back({start,end});
+                    t=1;
+                }
+            }
 
-         return dp;
-
+        }
+        if(t==0){
+            dp.push_back({start,end});
+        }
+        return dp;
     }
 };
