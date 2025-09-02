f5c66dcfd99e784e833de6fdc6f93b02df258814
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-02 14:05:22 +0530

    Time: 936 ms (5.25%) | Memory: 57.3 MB (32.65%) - LeetSync

---
+++ 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums, int k) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==k){
+                j=i;
+            }
+            if(nums[i]>k){
+                nums[i]=1;
+            }
+            else if(nums[i]==k){
+                nums[i]=0;
+            }
+            else{
+                nums[i]=-1;
+            }
+        }
+       
+        int i=j;
+        map<int,int> umap;
+        int sum=0;
+        int ans=0;
+        while(i<nums.size()){
+            sum+=nums[i];
+            if(sum==0 || sum==1){
+                ans+=1;
+            }
+            umap[sum]+=1;
+            i+=1;
+        }
+         for(auto x: umap){
+            cout<<x.first;
+            cout<<" ";
+            cout<<x.second;
+            cout<<"\n";
+        }
+        i=j-1;
+        cout<<ans;
+        cout<<"\n";
+        sum=0;
+ 
+        while(i>=0){
+         sum+=nums[i];
+         if(sum==0 || sum==1){
+            ans+=1;
+         }
+         ans+=umap[-sum];
+         if(sum==0){
+            ans-=1;
+         }
+         if(sum>0){
+            int val=sum-1;
+            ans+=umap[-val];
+            if(val==0){
+                ans-=1;
+            }
+         }
+         else if(sum<0){
+            int val=abs(sum);
+            val+=1;
+            ans+=umap[val];
+         }
+         else if(sum==0){
+            ans+=umap[1];
+         }
+         
+         i-=1;
+         cout<<sum;
+         cout<<" ";
+         cout<<ans;
+         cout<<"\n";
+        }
+
+       
+         return ans;
+    }
+};
