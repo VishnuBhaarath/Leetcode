e242d15b31915d1941837752bb24c688071793dd
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-14 22:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (35.22%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        vector<int> v;
-        int i=0;
-        while((i)<n){
-            if((i+1)<n){
-            if(nums[i]==nums[i+1]){
-                i+=2;
-            }
-            else{
-               v.push_back(nums[i]);
-               i+=1;
-               if(v.size()==2){
-                  return v;
-               }
-            }
-            }
-            else{
-                v.push_back(nums[i]);
-                return v;
-            }
+       long long int bit=0;
+        int one=0;
+        int two=0;
+        for(int i=0;i<nums.size();i++){
+            bit=bit^nums[i];
         }
+        cout<<bit;
+        cout<<" ";
+        bit=(bit&(bit-1))^(bit);
+        cout<<bit;
+        cout<<" ";
+        int j=0;
+        
+ 
+for (int i = 0; i <= 31; i++) {
+    if (bit & (1 << i)) {
+        j = i;
+        break;
+    }
+}
+        cout<<j;
+        cout<<"\n";
+      
+        for(int i=0;i<nums.size();i++){
+            if (nums[i] & (1 << j)) {
+               one=one^nums[i];
+         } else {
+               two=two^nums[i];
+          }
+        }
+    
+        vector<int> v;
+        v.push_back(one);
+        v.push_back(two);
+
         return v;
     }
 };
