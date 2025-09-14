c92cc4a1d23e253188779649d019def471fd6924
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-14 22:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (58.40%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -7,25 +7,16 @@ public:
         for(int i=0;i<nums.size();i++){
             bit=bit^nums[i];
         }
-        cout<<bit;
-        cout<<" ";
+      
         bit=(bit&(bit-1))^(bit);
-        cout<<bit;
-        cout<<" ";
+      
         int j=0;
         
  
-for (int i = 0; i <= 31; i++) {
-    if (bit & (1 << i)) {
-        j = i;
-        break;
-    }
-}
-        cout<<j;
-        cout<<"\n";
+
       
         for(int i=0;i<nums.size();i++){
-            if (nums[i] & (1 << j)) {
+            if (nums[i] & (bit)) {
                one=one^nums[i];
          } else {
                two=two^nums[i];
