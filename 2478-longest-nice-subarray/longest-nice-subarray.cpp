8cffa534dbc4558cf25e4e043f706784a6e0c347
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-16 10:23:18 +0530

    Time: 2 ms (42.05%) | Memory: 60.9 MB (89.26%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-    int check(vector<long long int> v){
-       
-        for(int i=0;i<v.size();i++){
-            for(int j=i+1;j<v.size();j++){
-            
-                if((v[i]&v[j])!=0){
-                     return -1;
-                   
-                }
-            }
-        }
-     
-        return v.size();
-       
-    }
     int longestNiceSubarray(vector<int>& nums) {
+        long long int sum=nums[0];
+        int i=1;
         long long int ans=1;
-        for(int i=0;i<nums.size();i++){
-            vector<long long int> v;
-            v.push_back(nums[i]);
-            for(int j=i+1;j<nums.size();j++){
-                v.push_back(nums[j]);
-                long long int val=check(v);
+        long long int lt=1;
+        long long int j=0;
+        while(i<nums.size()){
+           if((sum&nums[i])==0){
+             sum=sum^nums[i];
+             lt=(i-j)+1;
+             ans=max(ans,lt);
              
-                ans=max(ans,val);
-                if(val==-1){
+           }
+           else{
+             while((sum&nums[i])!=0){
+                sum=sum^nums[j];
+                j+=1;
+                if(j==i){
                     break;
                 }
-                if(ans==30){
-                    return ans;
-                }
-                if(v.size()==30){
-                    break;
-                }
-            }
-            
-        }
-    
-       
+             }
+             sum=sum|nums[i];
+           }
+           lt=i-j+1;
+           ans=max(ans,lt);
+           i+=1;
 
-        return ans;
+        }
+
+       return ans;
+
+
     }
 };
