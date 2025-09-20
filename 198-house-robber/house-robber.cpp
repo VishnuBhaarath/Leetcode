cc40e6837adb5fd8959faf272b00d6d192702b4f
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-20 11:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.72%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,25 +3,17 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,0);
-        int ans=0;
         dp[0]=nums[0];
-        ans=dp[0];
         if(n==1){
-            return dp[0];
+            return nums[0];
         }
         dp[1]=max(dp[0],nums[1]);
-        ans=max(ans,dp[1]);
+
         for(int i=2;i<nums.size();i++){
-           int sum=0;
-           for(int j=i-2;j>=0;j--){
-              int val=dp[j]+nums[i];
-              if(val>sum){
-                sum=val;
-              }
-           }
-           dp[i]=max(dp[i-1],sum);
+            int val1=nums[i]+dp[i-2];
+            int val2=dp[i-1];
+            dp[i]=max(val1,val2);
         }
-       
         return dp[n-1];
     }
 };
