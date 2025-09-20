586c3ef80e0ab1ff1e099aaefe385ef9334c4c9f
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-20 11:52:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (56.72%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,0);
+        if(n==1){
+            return nums[0];
+        }
+        dp[1]=nums[1];
+        if(n>2){
+        dp[2]=max(nums[2],dp[1]);
+        for(int i=3;i<nums.size();i++){
+            int val1=nums[i]+dp[i-2];
+            int val2=dp[i-1];
+            dp[i]=max(val1,val2);
+        }
+        }
+        int ans=dp[n-1];
+        for(int i=0;i<dp.size();i++){
+            cout<<dp[i];
+            cout<<" ";
+        }
+        cout<<"\n";
+        dp[0]=nums[0];
+        dp[1]=max(dp[0],nums[1]);
+        for(int i=2;i<(n-1);i++){
+            int val1=nums[i]+dp[i-2];
+            int val2=dp[i-1];
+            dp[i]=max(val1,val2);
+        }
+        for(int i=0;i<dp.size();i++){
+            cout<<dp[i];
+            cout<<" ";
+        }
+        cout<<"\n";
+        ans=max(ans,dp[n-2]);
+        return ans;
+    }
+};
