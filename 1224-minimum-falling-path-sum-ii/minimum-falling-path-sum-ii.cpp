78c3875b6d499868a519729da887820cf845335e
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-20 17:27:08 +0530

    Time: 102 ms (67.85%) | Memory: 19.8 MB (92.82%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -3,8 +3,7 @@ public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         int ans=0;
         int n=grid.size();
-        cout<<n;
-        cout<<"\n";
+      
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[i].size();j++){
                 if(i>0){
@@ -27,13 +26,7 @@ public:
             }
             
         }
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid.size();j++){
-                cout<<grid[i][j];
-                cout<<" ";
-            }
-            cout<<"\n";
-        }
+        
         return ans;
     }
 };
