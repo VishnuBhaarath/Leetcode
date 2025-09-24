fccf73346e05b1d1fc0ad5a5d1ee3df52de2832d
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-24 22:26:58 +0530

    Time: 28 ms (19.29%) | Memory: 17.5 MB (55.64%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,33 +4,29 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         vector<vector<int>> dp;
-        set<vector<int>> dp1;
+      
 
         for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
             for(int j=i+1;j<nums.size();j++){
+                if(j>(i+1) && nums[j]==nums[j-1]){
+                    continue;
+                }
                 int k=j+1;
                 int l=n-1;
                 
                 while(k<l){
-                   
-                    long long int sum=(long long)(nums[i])+(long long)(nums[j])+(long long)(nums[k])+(long long)(nums[l]);
+                   long long int sum=(long long)(nums[i])+(long long)(nums[j])+(long long)(nums[k])+(long long)(nums[l]);
                     
                    
                     if(sum==target){
-                        vector<int> v;
-                       
-                        v.push_back(nums[i]);
-                        v.push_back(nums[j]);
-                        v.push_back(nums[k]);
-                        v.push_back(nums[l]);
-                        if(dp1.find(v)!=dp1.end()){
-
-                        }
-                        else{
-                        dp.push_back(v);
-                        dp1.insert(v);}
+                        dp.push_back({nums[i],nums[j],nums[k],nums[l]});
                         l-=1;
                         k+=1;
+                        while(k<l && nums[k]==nums[k-1]) k+=1;
+                        while(k<l && nums[l]==nums[l+1]) l-=1;
                     }
                     else if(sum>target){
                         l-=1;
