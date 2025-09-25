8ee1443f0ad2a904e633ae53f02e1e6fe3a57cf4
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-25 10:55:04 +0530

    Time: 955 ms (5.09%) | Memory: 11.7 MB (82.93%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int cnt=0;
-    void func(vector<int>& nums, long long int target, long long int sum,int i){
+    int func(vector<int>& nums, long long int target, long long int sum,int i){
         if(i==nums.size()){
             if(target==sum){
-                cnt+=1;
+                return 1;
+            }else{
+                return 0;
             }
-            return;
         }
-        func(nums,target,sum+nums[i],i+1);
-        func(nums,target,sum-nums[i],i+1);
+        return func(nums,target,sum+nums[i],i+1)+func(nums,target,sum-nums[i],i+1);
 
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        func(nums,target,0,0);
-        return cnt;
+        return func(nums,target,0,0);
+        
     }
 };
