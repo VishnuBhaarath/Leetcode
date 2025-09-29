1931f9b14cf95a3bd812557acf57ac06ad219b00
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-09-29 09:37:55 +0530

    Time: 35 ms (21.08%) | Memory: 61.6 MB (5.11%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -23,7 +23,25 @@ public:
 
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        return func(amount,coins,0,0,dp);
+        if(amount==0){
+            return 1;
+        }
+        vector<vector<unsigned long long int>> dp(n+1,vector<unsigned long long int>(amount+1,0));
+        for(int i=n-1;i>=0;i--){
+            if(coins[i]<=amount){
+            dp[i][coins[i]]=1;}
+            for(int j=0;j<=amount;j++){
+
+               dp[i][j]+=dp[i+1][j];
+               if((j-coins[i])>=0){
+                dp[i][j]+= dp[i][j-coins[i]];
+               }
+               
+            }
+        }
+        
+       
+       
+        return dp[0][amount];
     }
 };
