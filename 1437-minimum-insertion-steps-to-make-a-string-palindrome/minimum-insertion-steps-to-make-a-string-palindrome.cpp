ef526a52c7fde5af0a684d399463f45cf18d734b
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-02 12:05:17 +0530

    Time: 23 ms (79.63%) | Memory: 31.5 MB (39.44%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
+    
+
     int minInsertions(string s) {
-         int n = s.size();
-        
-        // Create DP table
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        
-        // Base case: when j <= i, dp[i][j] = 0
-        // Already initialized to 0
-        
-        // Fill the DP table
-        // Since we go from i to i+1 and j to j-1, we need:
-        // - i goes from n-1 to 0 (backwards)
-        // - j goes from 0 to n-1 (forwards)
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = 0; j < n; j++) {
-                if(j <= i) {
-                    dp[i][j] = 0;  // Base case
-                } else if(s[i] == s[j]) {
-                    dp[i][j] = dp[i + 1][j - 1];
-                } else {
-                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
+        int n=s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(s[i]==s[j]){
+                    if((j-1)>=0){
+                    dp[i][j]=dp[i+1][j-1];
+                    }
+                }
+                else{
+                    int val=dp[i+1][j];
+                    if((j-1)>=0){
+                        val=min(val,dp[i][j-1]);
+                    }
+                    dp[i][j]=1+val;
                 }
             }
         }
-        
-        return dp[0][n - 1];
+      
+
+      return dp[0][n - 1];
     }
 };
