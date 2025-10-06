7840a62cef379e3bc604b774c89cd6dc3da4ef8d
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-06 21:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (17.74%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,52 +1,18 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int l=0;
-        int r=n-1;
-        int idx=-1;
         vector<int> v;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>target){
-                r=mid-1;
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-               if(idx==-1){
-                 idx=mid;
-               }
-               idx=min(idx,mid);
-               r=mid-1;
-            }
-        }
-        v.push_back(idx);
-        if(idx==-1){
-            v.push_back(idx);
+        int start=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        cout<<start;
+        if(start==nums.size() || nums[start]!=target){
+            v.push_back(-1);
+            v.push_back(-1);
             return v;
         }
-        idx=-1;
-        l=0;
-        r=n-1;
-         while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>target){
-                r=mid-1;
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-               if(idx==-1){
-                 idx=mid;
-               }
-               idx=max(idx,mid);
-               l=mid+1;
-            }
-        }
-        v.push_back(idx);
+        v.push_back(start);
+        int end=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        v.push_back(end-1);
+
         return v;
 
     }
