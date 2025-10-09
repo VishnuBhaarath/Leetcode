c75bfc7054742fabfac268714483ddafd07dafaa
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-09 12:39:37 +0530

    Time: 16 ms (15.90%) | Memory: 23 MB (43.35%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool check(vector<int>& piles,int m,int h){
+        int cnt=0;
+        for(int i=0;i<piles.size();i++){
+            int q=piles[i]/m;
+            cnt+=q;
+            if(piles[i]%m !=0){
+                cnt+=1;
+            }
+            if(cnt>h){
+                return false;
+            }
+        }
+        return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        sort(piles.begin(),piles.end());
+        int l=1;
+        int r=1e9;
+        int idx=-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(check(piles,m,h)){
+                if(idx==-1){
+                    idx=m;
+                }
+                else{
+                    idx=min(idx,m);
+                }
+                r=m-1;
+            } 
+            else{
+                l=m+1;
+            }
+
+        }
+         return idx;
+    }
+};
