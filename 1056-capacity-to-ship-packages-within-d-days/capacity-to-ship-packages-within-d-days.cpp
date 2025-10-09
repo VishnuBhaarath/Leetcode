3f416258d94b8f3b30f2a416e965a3e04a50a5ff
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-09 15:25:42 +0530

    Time: 9 ms (77.36%) | Memory: 35 MB (85.45%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int func(vector<int> &weights,int m,int days){
+        int sum=0;
+        int i=0;
+        int cnt=1;
+      
+        while(i< weights.size()){
+            if(weights[i]>m){
+                return -1;
+            }
+          
+           
+
+            if((sum+weights[i])<=m){
+                 sum+=weights[i];
+            }
+            else{
+                  cnt+=1;
+                  if(cnt>days){
+                   
+                    return -1;
+                  }
+                  sum=weights[i];
+            }
+          
+            i+=1;
+        }
+    
+        return cnt;
+
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+       
+        int r=0;
+        int l=0;
+        for(int i=0;i<weights.size();i++){
+            r+=weights[i];
+           
+        }
+        int ans=-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            int val=func(weights,m,days);
+            if(val==-1){
+                l=m+1;
+            }
+            else{
+                ans=m;
+                r=m-1;
+            }
+
+        }
+
+        return ans;
+    }
+};
