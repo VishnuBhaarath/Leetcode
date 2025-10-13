b2aab1f43ee0ca433b658810c9e9df5100ecf4b8
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-13 11:38:11 +0530

    Time: 8 ms (11.41%) | Memory: 20.8 MB (35.05%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
+     void bfs(vector<vector<int>>& adj,vector<bool>& visited,int i){
+        visited[i]=true;
+        queue<int> q;
+        q.push(i);
+        while(!q.empty()){
+            int currentNode=q.front();
+            q.pop();
+            for(int adjNode:adj[currentNode]){
+                if(!visited[adjNode]){
+                    visited[adjNode]=true;
+                    q.push(adjNode);
+                }
+            }
+        }
+        
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-    int n=isConnected.size();
-         vector<vector<int>> umap(n);
-        for(int i=0;i<isConnected.size();i++){
-            for(int j=0;j<isConnected[i].size();j++){
-                if(isConnected[i][j]==1){
-                    umap[i].push_back(j);
-                }
-            }
-        }
-
-        vector<int> v(n,0);
-        int cnt=0;
+        int ans=0;
+        int n=isConnected.size();
+        vector<bool> visited(n,false);
+        vector<vector<int>> adj(n);
         for(int i=0;i<n;i++){
-            if(v[i]==0){
-                v[i]+=1;
-                cnt+=1;
-                queue<int> q;
-                q.push(i);
-                while(!q.empty()){
-                    vector<int> v1= umap[q.front()];
-                    q.pop();
-                    for(int j=0;j<v1.size();j++){
-                        if(v[v1[j]]==0){
-                            v[v1[j]]+=1;
-                            q.push(v1[j]);
-                        }
-                    }
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j]==1){
+                    adj[i].push_back(j);
                 }
-
             }
         }
-        return cnt;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                ans++;
+                bfs(adj,visited,i);
+            }
+        }
+        return ans;
     }
 };
