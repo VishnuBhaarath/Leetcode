fbecaedddff6dadb63f66c5b1da40eb1066d73c9
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-13 10:56:39 +0530

    Time: 1009 ms (5.29%) | Memory: 371 MB (9.81%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        map<int,vector<int>> umap;
+        for(int i=0;i<edges.size();i++){
+            umap[edges[i][0]].push_back(edges[i][1]);
+            umap[edges[i][1]].push_back(edges[i][0]);
+        }
+        if(source==destination){
+            return true;
+        }
+        queue<int> q;
+        q.push(source);
+        map<int,int> umap1;
+        umap1[source]=1;
+        while(!q.empty()){
+             vector<int> v=umap[q.front()];
+             q.pop();
+             for(int i=0;i<v.size();i++){
+                if(v[i]==destination){
+                    return true;
+                }
+                if(umap1[v[i]]==0){
+                    q.push(v[i]);
+                    umap1[v[i]]+=1;
+                }
+             }
+        }
+
+        return false;
+    }
+};
