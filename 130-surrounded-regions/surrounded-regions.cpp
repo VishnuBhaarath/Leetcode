c78552e7c5cd472278a027761ef75eb2f91d4f74
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-14 09:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (71.87%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,98 +1,38 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-        
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        for(int i=1;i<(n-1);i++){
-            for(int j=1;j<(m-1);j++){
-                if(board[i][j]=='O'){
-                    if(dp[i][j]==0){
-                         queue<pair<int,int>> q;
-                         dp[i][j]=1;
-                         vector<pair<int,int>> v;
-                         q.push({i,j});
-                         v.push_back({i,j});
-                         int t=0;
-                        
-                         while(!q.empty()){
-                            int x=q.front().first;
-                            int y=q.front().second;
-                            
-                            q.pop();
-                            if(x-1 >=0){
-                            if(board[x-1][y]=='O'){
-                                if(dp[x-1][y]==0){
-                                    if(x-1 ==0){
-                                        t=1;
-                                    }
-                                    else{
-                                       v.push_back({x-1,y});
-                                    }
-                                    dp[x-1][y]=1;
-                                     q.push({x-1,y});
-                                }
-                            }
-                            }
-
-                            if(x+1 < n){
-                            if(board[x+1][y]=='O'){
-                                if(dp[x+1][y]==0){
-                                    if(x+1 == n-1){
-                                        t=1;
-                                    }
-                                    else{
-                                       v.push_back({x+1,y});
-                                    }
-                                    dp[x+1][y]=1;
-                                     q.push({x+1,y});
-                                }
-                            }
-                            }
-
-                               if(y-1 >= 0){
-                            if(board[x][y-1]=='O'){
-                                if(dp[x][y-1]==0){
-                                    if(y-1 == 0){
-                                        t=1;
-                                    }
-                                    else{
-                                       v.push_back({x,y-1});
-                                    }
-                                    dp[x][y-1]=1;
-                                     q.push({x,y-1});
-                                }
-                            }
-                            }
-
-                            if(y+1 < m){
-                            if(board[x][y+1]=='O'){
-                                if(dp[x][y+1]==0){
-                                    if(y+1 == m-1){
-                                        t=1;
-                                    }
-                                    else{
-                                       v.push_back({x,y+1});
-                                    }
-                                    dp[x][y+1]=1;
-                                     q.push({x,y+1});
-                                }
-                            }
-                            }
-                            
-                          
-                            
-                         }
-                       
-                         if(t==0){
-                            for(int i=0;i<v.size();i++){
-                                board[v[i].first][v[i].second]='X';
-                            }
-                         }
-                    }
-                }
+    vector<int> delrow = {-1, 0, 1, 0}, delcol = {0, 1, 0, -1};
+    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<char>>& board) {
+        int n = board.size(), m = board[0].size();
+        vis[r][c] = 1;
+        for(int i = 0; i < 4; i++) {
+            int nrow = r + delrow[i], ncol = c + delcol[i]; 
+            if(nrow>=0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && board[nrow][ncol] == 'O') {
+                dfs(nrow, ncol, vis, board); 
             }
         }
+        
+    }
+    void solve(vector<vector<char>>& board) {
+        int n = board.size(), m = board[0].size(); 
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+        for(int i = 0; i < m; i++) {
+            // first row
+            if(!vis[0][i] && board[0][i] == 'O') dfs(0, i, vis, board);
+            //last row
+            if(!vis[n-1][i] && board[n-1][i] == 'O') dfs(n-1, i, vis, board);
+        }
+
+        for(int i = 0; i < n; i++) {
+            // first col
+            if(!vis[i][0] && board[i][0] == 'O') dfs(i, 0, vis, board);
+            // last col
+            if(!vis[i][m-1] && board[i][m-1] == 'O') dfs(i, m-1, vis, board);
+        }
+
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < m; j++) {
+                if(!vis[i][j] && board[i][j] == 'O') board[i][j] = 'X';
+            }
+        } 
     }
 };
