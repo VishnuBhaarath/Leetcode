357173640188b9c1e108868161ca2deaf56e981b
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-15 15:04:16 +0530

    Time: 10 ms (25.06%) | Memory: 19.1 MB (85.83%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+    
+        for(int i=0;i<prerequisites.size();i++){
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+
+        vector<int> v(numCourses,0);
+        
+        queue<int> q;
+        for(int i=0;i<prerequisites.size();i++){
+            v[prerequisites[i][1]]+=1;
+        }
+        for(int i=0;i<v.size();i++){
+            if(v[i]==0){
+            q.push(i);}
+        }
+        int cnt=0;
+        while(!q.empty()){
+           
+            int tp=q.front();
+            q.pop();
+            cnt+=1;
+            for(int i=0;i<adj[tp].size();i++){
+                v[adj[tp][i]]-=1;
+                if(v[adj[tp][i]] == 0){
+                    q.push(adj[tp][i]);
+                }
+            }
+            
+           
+            
+        }
+        if(cnt==numCourses){
+            return true;
+        }
+        return false;
+    }
+};
