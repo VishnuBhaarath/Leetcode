be1cf7d2adc4e022dd269bf466beb6162c76392b
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-17 10:51:48 +0530

    Time: 110 ms (7.17%) | Memory: 12.4 MB (61.34%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-   
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
-       int n=arr.size();
-       vector<int> dp(n+1,0); 
-
-
-       for(int i=n-1;i>=0;i--){
-        int ans=INT_MIN;
-        int currMAX=arr[i];
-        for(int j = i; j < std::min(i+k, (int)(arr.size())); j++){
-            currMAX=max(currMAX,arr[j]);
-            int sum=(j-i+1)*currMAX+dp[j+1];
-         
-            ans=max(ans,sum);
+    int func1(int i,int j, vector<int>& arr){
+           int sz=(j-i)+1;
+           int temp=arr[i];
+           for(int k=i;k<=j;k++){
+               temp=max(temp,arr[k]);
+           }
+           return temp*sz;
+    }
+    int func(int n,int k, vector<int> & arr,int i,vector<int>& dp){
+        if(i==n){
+            return 0;
         }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int ans=0;
+        for(int j=i;j<min(n,i+k);j++){
+            int sum=func1(i,j,arr)+func(n,k,arr,j+1,dp);
+            ans=max(ans,sum);
+          
+            dp[i]=ans;
+        }
+     return ans;
+        
+    }
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n=arr.size();
+        vector<int> dp(n,-1);
+        int ans= func(n,k,arr,0,dp);
        
-        dp[i]=ans;
-      }
-     
-       return dp[0];
-      
+        return ans;
     }
 };
