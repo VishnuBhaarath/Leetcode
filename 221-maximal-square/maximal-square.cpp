817b50944a3c35e96d3c2ca995ac36d63e42adf7
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-21 20:38:48 +0530

    Time: 3 ms (93.03%) | Memory: 30.9 MB (77.93%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -31,13 +31,7 @@ public:
             }
         }
 
-        for(int i=0;i<dp.size();i++){
-            for(int j=0;j<dp[0].size();j++){
-                cout<<dp[i][j];
-                cout<<" ";
-            }
-            cout<<"\n";
-        }
+       
         return ans*ans;
     }
 };
