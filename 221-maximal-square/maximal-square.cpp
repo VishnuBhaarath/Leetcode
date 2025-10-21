33de1aac413cba942086a59cf4b9445631a9fee0
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-21 20:38:35 +0530

    Time: 1054 ms (5.00%) | Memory: 31.1 MB (39.36%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -3,28 +3,40 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>> dp(n, vector<int> (m,0));
-       
+        vector<vector<int>> dp(n,vector<int>(m,0));
         int ans=0;
-        
-        for(int i=0;i<dp.size();i++){
-          for(int j=0;j<dp[i].size();j++){
-            if(i==0 || j==0){
-                if(matrix[i][j]=='1'){
-                        ans=max(ans,1);
-                        dp[i][j]=1;
-                 }
-                }
-                else{
-                if(matrix[i][j]=='1'){
-                    ans=max(ans,1);
-                    int val=min(dp[i-1][j],dp[i][j-1]);
-                    val=min(val,dp[i-1][j-1]);
-                    dp[i][j]=1+val;
-                    ans=max(ans,dp[i][j]);
-                }}
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[i][0]=='1'){
+                dp[i][0]=1;
+                ans=1;
             }
-           
+        }
+        for(int j=0;j<matrix[0].size();j++){
+            if(matrix[0][j]=='1'){
+                dp[0][j]=1;
+                ans=1;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    int val=dp[i-1][j-1];
+                    val=min(val,dp[i][j-1]);
+                    val=min(val,dp[i-1][j]);
+                    ans=max(ans,val+1);
+                    dp[i][j]=val+1;
+                    
+                }
+            }
+        }
+
+        for(int i=0;i<dp.size();i++){
+            for(int j=0;j<dp[0].size();j++){
+                cout<<dp[i][j];
+                cout<<" ";
+            }
+            cout<<"\n";
         }
         return ans*ans;
     }
