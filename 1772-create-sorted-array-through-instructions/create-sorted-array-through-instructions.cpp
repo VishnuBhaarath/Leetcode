6fb4a63e88806c91530aa7dbf44bf4a79a95fd7f
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-22 23:40:42 +0530

    Time: 447 ms (43.60%) | Memory: 128.4 MB (60.67%) - LeetSync

--- 1772-create-sorted-array-through-instructions/create-sorted-array-through-instructions.cpp
+++ 1772-create-sorted-array-through-instructions/create-sorted-array-through-instructions.cpp
@@ -1,119 +1,85 @@
-class Solution {
-public:
-    void merge(vector<pair<int, pair<int, int>>> &v,int l,int m, int r){
-  
-        vector<pair<int, pair<int, int>>> left;
-        vector<pair<int, pair<int, int>>> right;
-      
-        for(int i=l;i<=m;i++){
-            left.push_back(v[i]);
-        }
-        for(int i=m+1;i<=r;i++){
-            right.push_back(v[i]);
-        }
+class SegmentTree {
+private:
+    vector<int> tree;
+    int n;
     
-      
-
-        for(int i=0;i<right.size();i++){
-            int num=right[i].first;
-            int l1=0;
-            int r1=left.size()-1;
-            int idx1=-1;
-            while(l1<=r1){
-                int m1=l1+(r1-l1)/2;
-
-                if(left[m1].first < num){
-                    idx1=m1;
-                    l1=m1+1;
-                }
-                else{
-                     r1=m1-1;
-                }
-            }
-            if(idx1!=-1){
-                right[i].second.second+=(idx1+1);
-            }
-            l1=0;
-            r1=left.size()-1;
-            idx1=-1;
-             while(l1<=r1){
-                int m1=l1+(r1-l1)/2;
-
-                if(left[m1].first > num){
-                    idx1=m1;
-                    r1=m1-1;
-                }
-                else{
-                     l1=m1+1;
-                }
-            }
-            if(idx1!=-1){
-                right[i].second.first+=(left.size()-idx1);
-            }
-        }
-
-      //   vector<pair<int, pair<int, int>>> temp;
-         int idx1=0;
-         int idx2=0;
-         while(idx1<left.size() && idx2<right.size()){
-            if(left[idx1].first < right[idx2].first){
-              //  temp.push_back(left[idx1]);
-                 v[l]=left[idx1];
-                idx1+=1;
-                l+=1;
-            }
-            else{
-              //  temp.push_back(right[idx2]);
-                v[l]=right[idx2];
-                l+=1;
-                idx2+=1;
-            }
-         }
-         while(idx1<left.size()){
-           //  temp.push_back(left[idx1]);
-             v[l]=left[idx1];
-             l+=1;
-                idx1+=1;
-         }
-         while(idx2<right.size()){
-          //  temp.push_back(right[idx2]);
-            v[l]=right[idx2];
-            l+=1;
-            idx2+=1;
-         }
-       
-     
-
-
-    }
-
-    void mergesort(vector<pair<int, pair<int, int>>> &v,int l, int r){
-        if(l>=r){
+    void update(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            tree[node] += val;
             return;
         }
-
-        int m=l+(r-l)/2;
-        mergesort(v,l,m);
-        mergesort(v,m+1,r);
-        merge(v,l,m,r);
-        return;
-    }
-    int createSortedArray(vector<int>& instructions) {
-        vector<pair<int, pair<int, int>>> v;
-        for(int i=0;i<instructions.size();i++){
-            v.push_back({instructions[i],{0,0}});
+        
+        int mid = start + (end - start) / 2;
+        if (idx <= mid) {
+            update(2 * node, start, mid, idx, val);
+        } else {
+            update(2 * node + 1, mid + 1, end, idx, val);
         }
-        int n=instructions.size();
-        mergesort(v,0,n-1);
-        int ans=0;
-        int mod=1e9+7;
-        for(int i=0;i<v.size();i++){
-            
-            ans+=min(v[i].second.first,v[i].second.second);
-            ans%=mod;
-       
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+    
+    int query(int node, int start, int end, int l, int r) {
+        if (r < start || l > end) {
+            return 0;
+        }
+        if (l <= start && end <= r) {
+            return tree[node];
         }
         
-        return ans;
+        int mid = start + (end - start) / 2;
+        int leftSum = query(2 * node, start, mid, l, r);
+        int rightSum = query(2 * node + 1, mid + 1, end, l, r);
+        return leftSum + rightSum;
+    }
+    
+public:
+    SegmentTree(int size) {
+        n = size;
+        tree.resize(4 * n, 0);
+    }
+    
+    void update(int idx, int val) {
+        update(1, 0, n - 1, idx, val);
+    }
+    
+    int query(int l, int r) {
+        if (l > r) return 0;
+        return query(1, 0, n - 1, l, r);
+    }
+};
+
+class Solution {
+public:
+    int createSortedArray(vector<int>& instructions) {
+        int n = instructions.size();
+        if (n == 0) return 0;
+        
+        // Find the maximum value to determine segment tree size
+        int maxVal = *max_element(instructions.begin(), instructions.end());
+        
+        // Create segment tree with size maxVal+1 to handle 1-indexed values
+        SegmentTree segTree(maxVal + 1);
+        
+        long long cost = 0;
+        int mod = 1e9 + 7;
+        
+        for (int i = 0; i < n; i++) {
+            int num = instructions[i];
+            
+            // Count elements strictly less than num (values 1 to num-1)
+            int smaller = (num > 1) ? segTree.query(1, num - 1) : 0;
+            
+            // Count elements strictly greater than num (values num+1 to maxVal)
+            int greater = (num < maxVal) ? segTree.query(num + 1, maxVal) : 0;
+            
+            // Add the minimum cost
+            cost += min(smaller, greater);
+            cost %= mod;
+            
+            // Update the segment tree with current element
+            segTree.update(num, 1);
+        }
+        
+        return cost;
     }
 };
