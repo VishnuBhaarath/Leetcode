94939cd5345c88302d608bafd9fb8ff546a3a79d
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-22 09:49:34 +0530

    Time: 1 ms (25.63%) | Memory: 26.1 MB (50.40%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,68 +1,38 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
+    int trap(vector<int>& arr) {
+        int n=arr.size();
         stack<int> s;
-        vector<int> left(n,0);
+        int i=0;
+        int j=n-1;
+        int leftmax=arr[i];
+        int rightmax=arr[j];
         int ans=0;
-        for(int i=0;i<height.size();i++){
+        i+=1;
+        j-=1;
+      
+        while(i<=j){
+           if(leftmax<=rightmax){
+               if(arr[i]<leftmax){
+                ans+=(leftmax-arr[i]);
+               }
+
+               leftmax=max(leftmax,arr[i]);
+                i+=1;
+           }
+           else {
+              if(arr[j]<rightmax){
+                ans+=(rightmax-arr[j]);
+              }
+            
+              rightmax=max(rightmax,arr[j]); 
+               j-=1;
+           }
           
-            if(s.empty()){
-                s.push(height[i]);
-            }
-            else if(height[i]<s.top()){
-              
-                left[i]=s.top()-height[i];
-            }
-            else{
-                while(height[i]>=s.top()){
-                    s.pop();
-                    if(s.empty()){
-                        break;
-                    }
-                }
-                if(s.empty()){
-                    s.push(height[i]);
-                }
-                else{
-                    left[i]=s.top();
-                }
-            }
-           
+            
         }
+    return ans;
 
-        for(int i=0;i<left.size();i++){
-            cout<<left[i];
-            cout<<" ";
-        }
-        vector<int> right(n,0);
-        stack<int> s1;
-        for(int i=n-1;i>=0;i--){
-            if(s1.empty()){
-                s1.push(height[i]);
-            }
-            else if(height[i]<s1.top()){
-                right[i]=s1.top()-height[i];
-            }
-            else{
-                while(height[i]>=s1.top()){
-                    s1.pop();
-                    if(s1.empty()){
-                        break;
-                    }
-                }
-                 if(s1.empty()){
-                    s1.push(height[i]);
-                }
-                else{
-                    right[i]=s1.top();
-                }
-            }
-
-        }
-        for(int i=0;i<left.size();i++){
-            ans+=min(left[i],right[i]);
-        }
-        return ans;
+        
     }
 };
