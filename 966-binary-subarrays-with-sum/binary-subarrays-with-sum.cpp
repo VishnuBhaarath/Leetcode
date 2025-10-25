b10cf03fb03adf992c6ae07d7304b2c7d0bf92c3
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-25 09:41:39 +0530

    Time: 49 ms (5.01%) | Memory: 41.7 MB (6.60%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-    int func1(vector<int> nums, int goal){
-        int i=0;
-        int j=0;
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        map<int,int> umap;
+        umap[0]+=1;
         int sum=0;
         int cnt=0;
-        while(j<nums.size()){
-           sum+=nums[j];
-           if(sum<=goal){
-             cnt+=(j-i)+1;
-           }
-           else{
-            while(sum>goal){
-                if(i<nums.size()){
-                sum-=nums[i];
-                i+=1;}
-                else{
-                    return 0;
-                }
-            }
-            cnt+=(j-i)+1;
-           }
-           j+=1;
+        for(auto it:nums){
+            sum+=it;
+            cnt+=umap[sum-goal];
+            umap[sum]+=1;
         }
         return cnt;
-    }
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int sum1=func1(nums,goal);
-        int sum2=func1(nums,goal-1);
-        
-        return sum1-sum2;
-
     }
 };
