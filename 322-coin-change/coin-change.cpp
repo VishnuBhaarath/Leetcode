93a970982b179ef53ed30747a1e046a4f01c301d
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-27 09:45:15 +0530

    Time: 23 ms (69.85%) | Memory: 17.8 MB (83.06%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        sort(coins.begin(),coins.end());
         vector<int> dp(amount+1,0);
         if(amount==0){
             return 0;
         }
         dp[0]=1;
-        for(int i=0;i<=amount;i++){
-            if(dp[i]!=0){
+        for(int i=1;i<=amount;i++){
             for(int j=0;j<coins.size();j++){
-                if(coins[j]<=amount){
-                long long int k=(long long)(i+coins[j]);
-                if(k<=amount){
-                    if(dp[k]==0){
-                        dp[k]=1+dp[i];
-                        if(i==0){
-                            dp[k]=1;
+                if(coins[j]>i){
+                    break;
+                }
+                if(i==coins[j]){
+                    dp[i]=1;
+                    break;
+                }
+                else{
+                    if(dp[i-coins[j]]>0){
+                        if(dp[i]==0){
+                            dp[i]=dp[i-coins[j]]+1;
+                        }
+                        else{
+                            dp[i]=min(dp[i],dp[i-coins[j]]+1);
                         }
                     }
-                    dp[k]=min(dp[k],1+dp[i]);
                 }
-                }
-            }
             }
         }
+        
         if(dp[amount]==0){
             return -1;
         }
