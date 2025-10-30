17341577c35359d65c817bf0190e0db9fc7fa142
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-10-30 12:17:09 +0530

    Time: 209 ms (5.00%) | Memory: 22.9 MB (52.78%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int i=1;
-        int sum=1;
         int n=ratings.size();
-        while(i<n){
-            while(i<n && ratings[i]==ratings[i-1]){
-                sum+=1;
-                i+=1;
-            
-            }
-            int peak=1;
-            while(i<n && ratings[i] > ratings[i-1]){
-                peak+=1;
-                sum+=peak;
-                i+=1;
-            }
-            int down=1;
-            while(i<n && ratings[i]<ratings[i-1]){
-                sum+=down;
-                i+=1;
-                down+=1;
-            }
-            if(down>peak){
-                sum+=(down-peak);
+        vector<int> v(n,1);
+        for(int i=1;i<ratings.size();i++){
+             if(ratings[i]>ratings[i-1]){
+                v[i]=v[i-1]+1;
+             }
+        }
+        for(int i=0;i<v.size();i++){
+            cout<<v[i];
+            cout<<" ";
+        }
+        for(int i=n-1;i>0;i--){
+            if(ratings[i-1]>ratings[i]){
+                if(v[i-1]<=v[i]){
+                    v[i-1]=1+v[i];
+                }
+                if((i-2)>=0){
+                    if(ratings[i-2]>ratings[i-1]){
+                        if(v[i-2]<=v[i-1]){
+                            v[i-2]=v[i-1]+1;
+                        }
+                    }
+                }
             }
         }
+        int sum=0;
+
+        for(int i=0;i<v.size();i++){
+           
+            sum+=v[i];
+        }
+        
+   
+
         return sum;
     }
 };
