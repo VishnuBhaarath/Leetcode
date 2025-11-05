80e26cf4fef25e7bb36f6c96d265f287ed7d2983
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-11-05 09:46:38 +0530

    Time: 104 ms (7.91%) | Memory: 12.4 MB (61.82%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-    int func1(int i,int j, vector<int>& arr){
-           int sz=(j-i)+1;
-           int temp=arr[i];
-           for(int k=i;k<=j;k++){
-               temp=max(temp,arr[k]);
-           }
-           return temp*sz;
+    int func1(int i,int j,vector<int>&arr){
+        int val=arr[i];
+        for(int k=i;k<=j;k++){
+            val=max(val,arr[k]);
+        }
+       
+        return (j-i+1)*val;
     }
-    int func(int n,int k, vector<int> & arr,int i,vector<int>& dp){
-        if(i==n){
+    int func(vector<int>&arr,int i,int k,int n,vector<int>& dp){
+        if(i>=n){
             return 0;
         }
         if(dp[i]!=-1){
             return dp[i];
         }
         int ans=0;
-        for(int j=i;j<min(n,i+k);j++){
-            int sum=func1(i,j,arr)+func(n,k,arr,j+1,dp);
+        for(int j=i;j<min(i+k,n);j++){
+            int sum=func1(i,j,arr)+func(arr,j+1,k,n,dp);
             ans=max(ans,sum);
-          
-            dp[i]=ans;
+           
+            
         }
-     return ans;
-        
+        return dp[i]=ans;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<int> dp(n+1,0);
-        int ans= INT_MIN;
-
-        for(int i=n-1;i>=0;i--){
-            int val=arr[i];
-             for(int j=i;j<min(n,i+k);j++){
-                   val=max(val,arr[j]);
-                   int sz=(j-i)+1;
-                   int sum=val*sz+dp[j+1];
-
-                    ans=max(ans,sum);
-                     dp[i]=ans;
-             }   
-            
-        }
-       
-        return ans;
+        vector<int> dp(n,-1);
+        return func(arr,0,k,n,dp);
     }
 };
