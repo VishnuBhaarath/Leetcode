50f234a995f00f053f5fef97bbcb62a6a004706b
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-11-05 21:52:55 +0530

    Time: 37 ms (38.13%) | Memory: 13.9 MB (52.87%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
-    int func(vector<int>& cuts, int i, int j, int n, vector<vector<int>>& dp) {
-        if (cuts[i] >= cuts[j]) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int ans = INT_MAX;
-        for (int k = 0; k < cuts.size(); k++) {
-            if (cuts[k] > cuts[i] && cuts[k] < cuts[j]) {
-
-                int sum = (cuts[j] - cuts[i]) + func(cuts, i, k, n, dp) +
-                          func(cuts, k, j, n, dp);
-                ans = min(ans, sum);
-            }
-        }
-        if (ans == INT_MAX) {
-            return 0;
-        }
-
-        return dp[i][j] = ans;
-    }
+   
     int minCost(int n, vector<int>& cuts) {
         //  sort(cuts.begin(), cuts.end());
         int i = 0;
