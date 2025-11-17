52535ee49595c69542852408e290b1aebc6321bb
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-11-17 09:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (64.68%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -15,22 +15,34 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         stack<TreeNode*> st;
         vector<int> v;
-        if(root!=NULL)
-        st.push(root);
-        map<TreeNode*,int> umap;
+        if (root != NULL)
+            st.push(root);
+
         while (!st.empty()) {
             TreeNode* tp = st.top();
-            
-            if(tp->left!=NULL && umap[tp->left]==0){
+
+            if (tp->left != NULL) {
                 st.push(tp->left);
-                umap[tp->left]=1;
-            }
-            else{
+
+            } else {
                 v.push_back(tp->val);
                 st.pop();
-                if(tp->right!=NULL && umap[tp->right]==0){
-                     st.push(tp->right);
-                     umap[tp->right]=1;
+                if (tp->right != NULL) {
+                    st.push(tp->right);
+
+                } else {
+                    while (!st.empty()) {
+                        if (st.top()->right == NULL) {
+                            v.push_back(st.top()->val);
+                            st.pop();
+                        } else {
+                            TreeNode* tp1 = st.top();
+                            v.push_back(st.top()->val);
+                            st.pop();
+                            st.push(tp1->right);
+                            break;
+                        }
+                    }
                 }
             }
         }
