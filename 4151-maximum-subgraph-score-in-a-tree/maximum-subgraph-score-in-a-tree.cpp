3a26f38782fae6b41e43fe48010f539d802cc689
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-12-17 09:48:00 +0530

    Time: 328 ms (45.25%) | Memory: 387.2 MB (74.84%) - LeetSync

---
+++ 4151-maximum-subgraph-score-in-a-tree/maximum-subgraph-score-in-a-tree.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+  vector<vector<int>> adj;
+    vector<int> dp, res;
+
+    void dfs1(int u, int p, vector<int>& good) {
+        dp[u] = good[u];
+
+        for(int& v : adj[u]) {
+            if(v != p) {
+                dfs1(v, u, good);
+                dp[u] += max(0, dp[v]);
+            }
+        }
+    }
+
+    void dfs2(int u, int p, int sum) {
+        res[u] = dp[u] + max(0, sum);
+
+        for(int& v : adj[u])
+            if(v != p)
+                dfs2(v, u, res[u] - max(0, dp[v]));
+    }
+    vector<int> maxSubgraphScore(int n, vector<vector<int>>& edges, vector<int>& good) {
+        adj.resize(n); dp.resize(n); res.resize(n);
+
+        for(auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        for(int& g : good)
+            g = g ? 1 : -1;
+
+        dfs1(0, -1, good);
+        dfs2(0, -1, 0);
+
+        return res;
+    }
+};
