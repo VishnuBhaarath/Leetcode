640c8d69a628de8cd7e63d6a067c9b7bda7f80b5
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-12-18 11:22:27 +0530

    Time: 1577 ms (5.05%) | Memory: 387.8 MB (69.86%) - LeetSync

--- 4151-maximum-subgraph-score-in-a-tree/maximum-subgraph-score-in-a-tree.cpp
+++ 4151-maximum-subgraph-score-in-a-tree/maximum-subgraph-score-in-a-tree.cpp
@@ -1,43 +1,60 @@
 class Solution {
 public:
-  vector<vector<int>> adj;
-    vector<int> dp, res;
+    vector<int> dp;
+    vector<vector<int>> adj;
+    vector<int> res;
 
-    void dfs1(int u, int p, vector<int>& good) {
-        dp[u] = good[u];
-
-        for(int& v : adj[u]) {
-            if(v != p) {
-                dfs1(v, u, good);
-                dp[u] += max(0, dp[v]);
+    void dfs1(int u,int p,int sum,vector<int>&good){
+        dp[u]=good[u];
+        for(int i=0;i<adj[u].size();i++){
+            if(adj[u][i]!=p){
+                dfs1(adj[u][i],u,sum,good);
+               
+              
+                dp[u]=max(dp[u],dp[u]+max(0,dp[adj[u][i]]));
+           
             }
         }
+
         return;
     }
 
-    void dfs2(int u, int p, int sum) {
-        res[u] = dp[u] + max(0, sum);
+    void dfs2(int u,int p,int sum,vector<int> & good){
+        res[u]=dp[u]+ max(0,sum);
 
-        for(int& v : adj[u])
-            if(v != p)
-                dfs2(v, u, res[u] - max(0, dp[v]));
+        for(int i=0;i<adj[u].size();i++){
+            if(adj[u][i]!=p){
+                dfs2(adj[u][i],u,res[u]-max(0,dp[adj[u][i]]),good);
+            }
+        }
+    return;
     }
+
     vector<int> maxSubgraphScore(int n, vector<vector<int>>& edges, vector<int>& good) {
-        adj.resize(n); dp.resize(n); res.resize(n);
+        adj.resize(n);
+        res.resize(n,0);
+        dp.resize(n,0);
 
-        for(auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
         }
 
-        for(int& g : good)
-            g = g ? 1 : -1;
-
-        dfs1(0, -1, good);
-        dfs2(0, -1, 0);
-
+        for(int i=0;i<good.size();i++){
+            if(good[i]==0){
+                good[i]=-1;
+            }
+            else{
+                good[i]=1;
+            }
+        }
+        dfs1(0,-1,0,good);
+        for(int i=0;i<dp.size();i++){
+            cout<<dp[i];
+            cout<<" ";
+        }
+        dfs2(0,-1,-1,good);
+        
         return res;
     }
 };
