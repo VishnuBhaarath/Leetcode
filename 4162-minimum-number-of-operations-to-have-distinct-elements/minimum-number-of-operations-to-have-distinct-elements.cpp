72eaa1af2679ff94421a12fcdc30c1f6268ea2d6
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-12-26 11:32:15 +0530

    Time: 788 ms (10.10%) | Memory: 265.8 MB (16.61%) - LeetSync

---
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        map<int,int> umap;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            umap[nums[i]]+=1;
+        }
+        for(auto x:umap){
+            if(x.second>1){
+                cnt+=1;
+            }
+        }
+        cout<<cnt;
+        cout<<"\n";
+        if(cnt==0){
+            return 0;
+        }
+        int ops=1;
+        int t=0;
+        for(int i=0;i<nums.size();i++){
+             if(umap[nums[i]]==2){
+              //  umap[nums[i]]-=1;
+                cnt-=1;
+             }
+           
+             umap[nums[i]]-=1;
+             if(cnt==0){
+                return ops;
+             }
+              t+=1;
+              if(t==3){
+                ops+=1;
+                t=0;
+              }
+        }
+return ops;
+    }
+};
