dff7cc77f843752bac22ab0bf3e6ae83de6f531c
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-12-27 12:22:37 +0530

    Time: 68 ms (28.05%) | Memory: 74.9 MB (83.93%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -5,10 +5,11 @@ public:
         sort(A.begin(), A.end());
         int i = 0, res = 0, n = A.size();
         for (int d = 1; d <= 100000; ++d) {
-            while (pq.size() && pq.top() < d)
-                pq.pop();
-            while (i < n && A[i][0] == d)
-                pq.push(A[i++][1]);
+            while (pq.size() && pq.top() < d){
+                pq.pop();}
+            while (i < n && A[i][0] == d){
+                pq.push(A[i++][1]);}
+                
             if (pq.size()) {
                 pq.pop();
                 ++res;
