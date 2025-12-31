5b44e14048405a4bc634129b3e9514dfd209e5ba
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2025-12-31 18:34:54 +0530

    Time: 147 ms (10.71%) | Memory: 108.2 MB (5.05%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
+    vector<long long int> parent; unordered_map<long long int, long long int> umap;
     int longestConsecutive(vector<int>& nums) {
-        map<int,int> umap;
-        for(int i=0;i<nums.size();i++){
-            umap[nums[i]]+=1;
+        int n = nums.size();
+        if(nums.size()==0){
+            return 0;
         }
-        int ans=0;
-        map<int,int> umap1;
-        for(int i=0;i<nums.size();i++){
-            if(umap[nums[i]-1]==0 && umap1[nums[i]]==0){
-                int cnt=1;
-                umap1[nums[i]]+=1;
-                int j=nums[i]+1;
-                while(umap[j]>0){
-                    cnt+=1;
-                    j+=1;
-                }
-                ans=max(ans,cnt);
+        parent.resize(n, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            umap[nums[i]] =i;
+            parent[i] = nums[i];
+        }
+        long long int ans = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+          
+            if(umap.find(nums[i]-1)!=umap.end()){
+                int idx=umap[nums[i]-1];
+                if(parent[idx]== nums[i]-1){
+                   find(i,nums);}
+                   else{
+                    parent[i]=parent[idx];
+                   }
             }
+           
         }
+       // long long int ans=1;
+        for(int i=0;i<parent.size();i++){
+          
+            long long int val=nums[i]-parent[i]+1;
+            ans=max(ans,val);
+         
+        }
+    
+
         return ans;
+    }
+    int find(int x,vector<int>&nums) { 
+        if(umap.find(nums[x]-1)==umap.end()){
+            return parent[x]=nums[x];
+        }
+        return parent[x] = find(umap[nums[x]-1],nums);
+         
     }
 };
