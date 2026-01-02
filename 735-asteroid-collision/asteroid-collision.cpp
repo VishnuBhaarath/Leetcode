4f40f60c2f31e81001a63877642c3634a63d503b
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-02 22:00:21 +0530

    Time: 4 ms (14.83%) | Memory: 22 MB (55.99%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -2,55 +2,48 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> s;
-        s.push(asteroids[0]);
-
-        for(int i=1;i<asteroids.size();i++){
+        for(int i=0;i<asteroids.size();i++){
             if(s.empty()){
                 s.push(asteroids[i]);
             }
-            else if((asteroids[i]>0 && s.top()>0) || (asteroids[i]<0 && s.top()<0) || (s.top()<0 && asteroids[i]>0)){
-                s.push(asteroids[i]);
-            }
-            else{
-                while(1){
-                    int tp=s.top();
-                    s.pop();
-                    int t=0;
-                    if(tp>0 && asteroids[i]<0){
-                        if(abs(asteroids[i]) < abs(tp)){
-                            s.push(tp);
+            else if(asteroids[i]<0){
+                 if(s.top()<0){
+                    s.push(asteroids[i]);
+                 }
+                 else{
+                     while(s.top()>0){
+                         if(s.top()>abs(asteroids[i])){
+                             break;
+                         }
+                         else if(s.top()==abs(asteroids[i])){
+                            s.pop();
                             break;
-                        }
-                        else if(abs(asteroids[i]) == abs(tp)){
-                           
-                                break;
-                            
-                        }
-                        else if(abs(asteroids[i]) > abs(tp)){
-                            if(s.empty()){
+                         }
+                         else{
+                             s.pop();
+                             if(s.empty()){
                                 s.push(asteroids[i]);
-                                    break;
-                                
-                            }
-                        }
-                    }
-                    else{
-                        s.push(tp);
-                        s.push(asteroids[i]);
-                        break;
-                    }
-                }
-                
+                                break;
+                             }
+                             if(s.top()<0){
+                                s.push(asteroids[i]);
+                                break;
+                             }
+                         }
+                     }
+                 }
             }
-    }
-  
-    vector<int> v(s.size(),-1);
-    int m=s.size();
-    while(!s.empty()){
-        v[m-1]=s.top();
-        s.pop();
-        m-=1;
-    }
-    return v;
+            else if(asteroids[i]>0){
+                 s.push(asteroids[i]);
+            }
+        }
+        vector<int> ans(s.size(),0);
+        int j=s.size()-1;
+        while(!s.empty()){
+            ans[j]=s.top();
+            s.pop();
+            j-=1;
+        }
+        return ans;
     }
 };
