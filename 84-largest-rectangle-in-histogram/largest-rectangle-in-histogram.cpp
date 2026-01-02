fb55eec7a9bb2e987090b65e2347f74143a69c1c
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-02 11:48:58 +0530

    Time: 29 ms (32.54%) | Memory: 85.9 MB (23.90%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,57 +1,70 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        int i = 0;
+        int area=0;
+        int n=heights.size();
+        int marea=0;
         stack<int> s;
-        int ans = 0;
-        for (int i = 0; i < heights.size(); i++) {
-            if (s.empty()) {
+        for(int i=0;i<heights.size();i++){
+           if(s.empty()){
+             s.push(i);
+           }
+           else{
+              if(heights[i]>=heights[s.top()]){
                 s.push(i);
-            } else {
-                if (heights[i] >= heights[s.top()]) {
-                    s.push(i);
-                } else {
-                    while (heights[i] < heights[s.top()]) {
-                        int tp = s.top();
-                        s.pop();
-                        int tp1 = -1;
-                        if (!s.empty()) {
-                            tp1 = s.top();
-                        }
+              }
+              else{
+                while(heights[s.top()]> heights[i]){
+                     int tp=s.top();
+                     int h=heights[s.top()];
+                    
+                     int w=i-s.top();
+                      s.pop();
+                      int tp1=-1;
+                      if(!s.empty()){
+                        tp1=s.top();
+                      }
+                      w+=(tp-tp1-1);
 
-                        int w = (i - tp1 - 1);
-                        int h = heights[tp];
-                        int area = w * h;
-                        ans = max(ans, area);
-                      
-
-                        if (s.empty()) {
-                            break;
-                        }
-                    }
-                    s.push(i);
+                     int area=h*w;
+                     marea=max(marea,area);
+                     
+                     if(s.empty()){
+                        break;
+                     }
                 }
-            }
+                s.push(i);
+                
+              }
+           }
         }
-        while (!s.empty()) {
-            int tp = s.top();
-            s.pop();
-            int tp1 = -1;
-            if (!s.empty()) {
-                tp1 = s.top();
-            }
-            int w = (n - tp1-1);
-            int h = heights[tp];
-            int area = w * h;
-            ans = max(ans, area);
-            
-
-            if (s.empty()) {
-                break;
-            }
+       
+        vector<int> v;
+        while(!s.empty()){
+           v.push_back(s.top());
+           s.pop();
         }
+        int m=v.size();
+        reverse(v.begin(),v.end());
+        int r=v[m-1];
+        int l=0;
+        for(int i=m-1;i>=0;i--){
+              int w=r-v[i];
+              w+=1;
+              if(i==0){
+                w+=(v[i]-l);
+              }
+              else{
+                int d=v[i]-v[i-1];
+                d-=1;
+                w+=d;
+              }
+              int h=heights[v[i]];
+              int area=h*w;
+             
+              marea=max(marea,area);
 
-        return ans;
+        }
+        return marea;
     }
 };
