aea93747daac3337f68d556452f79b329738e735
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-03 17:13:49 +0530

    Time: 195 ms (12.84%) | Memory: 204.1 MB (8.03%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,46 +1,55 @@
 class Solution {
 public:
-   
-    bool canPartition(vector<int>& arr) {
-         int n=arr.size();
-         int m=0;
-         for(int i=0;i<arr.size();i++){
-            m+=arr[i];
-         }
-         if((m%2)!=0){
+    bool check(int i, vector<int> & nums, int sum1,int sum2){
+        if(i==nums.size()){
+            if(sum1==sum2){
+                return true;
+            }
             return false;
-         }
-
-         int val=m/2;
-        
-        vector<vector<int>> dp(n,vector<int>(m+1,-1));
-        for(int i=n-1;i>=0;i--){
-            for(int j=m;j>=0;j--){
-                if(i==(n-1)){
-                   
-                    dp[i][arr[i]]=1;
+        }
+        return check(i+1,nums,nums[i]+sum1,sum2) || check(i+1,nums,sum1,sum2+nums[i]);
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(sum%2 !=0){
+           return false;
+        }
+        int val=sum/2;
+       
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(sum+1,0));
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<=sum;j++){
+                if(nums[i]==val){
+                  
+                     return true;
+                }
+                if(i==0){
+                    dp[i][nums[i]]=1;
+                    
                 }
                 else{
-                    if(dp[i+1][j]==1){
+                    dp[i][nums[i]]=1;
+                    if(dp[i-1][j]==1){
                         dp[i][j]=1;
-                        dp[i][j+arr[i]]=1;
-                        if((j+arr[i]) ==val){
-                         
+                    }
+                    if(j+nums[i]<=sum){
+                        
+                        if(dp[i-1][j]==1){
+                            if((j+nums[i])==val){
                            return true;
                         }
+                            dp[i][j+nums[i]]=1;
+                        }
                     }
-                    if((arr[i]) ==val){
-                   
-                        return true;
-                    }
-                    dp[i][arr[i]]=1;
                 }
             }
-        } 
-        if(dp[0][val]==1){
-            return true;
         }
         
-         return false;
+        return false;
+      // return check(0,nums,0,0);
     }
 };
