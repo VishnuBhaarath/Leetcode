be397e7c399a9ef9cfdc38f46fb2e47b942ddd84
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-04 09:23:31 +0530

    Time: 55 ms (66.95%) | Memory: 14.3 MB (70.97%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,20 +3,16 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,0);
-        dp[0]=1;
         int ans=1;
-        for(int i=1;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
+            dp[i]=1;
             for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i],dp[j]+1);
-                    ans=max(ans,dp[i]);
-                }
-                else{
-                    dp[i]=max(dp[i],1);
-                }
+              if(nums[i]>nums[j]){
+                dp[i]=max(dp[i],1+dp[j]);
+              }
             }
+            ans=max(ans,dp[i]);
         }
-        
         return ans;
     }
 };
