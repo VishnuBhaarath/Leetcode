a1eabaabe272b62b387e026723c7c0e73c6727e1
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-07 12:47:35 +0530

    Time: 27 ms (57.92%) | Memory: 17.8 MB (79.87%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-       // sort(coins.begin(),coins.end());
-        vector<int> dp(amount+1,0);
+        sort(coins.begin(),coins.end());
         if(amount==0){
             return 0;
         }
-        dp[0]=1;
-        for(int i=1;i<=amount;i++){
+        vector<int> dp(amount+1,0);
+        for(int i=0;i<coins.size();i++){
+            if(coins[i]<dp.size()){
+                dp[coins[i]]=1;
+            }
+        }
+        for(int i=1;i<dp.size();i++){
             for(int j=0;j<coins.size();j++){
-                if(coins[j]>i){
-                    continue;
-                }
-                if(i==coins[j]){
-                    dp[i]=1;
-                    break;
-                }
-                else{
-                    if(dp[i-coins[j]]>0){
-                        if(dp[i]==0){
-                            dp[i]=dp[i-coins[j]]+1;
+                if(dp[i]>0){
+                    if((long long)(i)+(long long)(coins[j]) <= (long long)(amount)){
+                        if(dp[i+coins[j]]==0){
+                            dp[i+coins[j]]=dp[i]+1;
                         }
                         else{
-                            dp[i]=min(dp[i],dp[i-coins[j]]+1);
+                            dp[i+coins[j]]=min(dp[i+coins[j]],dp[i]+1);
                         }
                     }
                 }
             }
         }
-        
+       
         if(dp[amount]==0){
             return -1;
         }
