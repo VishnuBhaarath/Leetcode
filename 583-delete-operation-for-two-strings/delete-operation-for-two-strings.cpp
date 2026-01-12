3485adc5722db306e1cd2249d0db17d9385fce4b
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-12 09:32:13 +0530

    Time: 683 ms (5.02%) | Memory: 377.8 MB (5.02%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,47 +1,31 @@
 class Solution {
 public:
-    int func(string &word1, string &word2, int i,int j,vector<vector<int>>dp){
+
+    int check(string word1,string word2,int i ,int j,vector<vector<int>> &dp){
         if(i==word1.size() && j==word2.size()){
             return 0;
         }
-        if(i==word1.size()){
+        if(i==word1.size() && j!=word2.size()){
             return word2.size()-j;
         }
-        if(j==word2.size()){
+        if(i!=word1.size() && j==word2.size()){
             return word1.size()-i;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-
         if(word1[i]==word2[j]){
-            return dp[i][j]=func(word1,word2,i+1,j+1,dp);
+            return dp[i][j]=check(word1,word2,i+1,j+1,dp);
         }
         else{
-            return dp[i][j]=1+min(func(word1,word2,i+1,j,dp),func(word1,word2,i,j+1,dp));
+            return dp[i][j]=min(1+check(word1,word2,i+1,j,dp),1+check(word1,word2,i,j+1,dp));
         }
-        return 0;
+return 0;
     }
-    
     int minDistance(string word1, string word2) {
-       int n=word1.size();
-       int m=word2.size();
-       int ans=0;
-       vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-       for(int i=0;i<word1.size();i++){
-        for(int j=0;j<word2.size();j++){
-            if(word1[i]==word2[j]){
-              dp[i+1][j+1]=1+dp[i][j];
-              ans=max(ans,dp[i+1][j+1]);
-            }
-            else{
-              dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j]);
-              ans=max(ans,dp[i+1][j+1]);
-            }
-        }
-       }
- 
-     
-       return n+m-(2*ans);
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return check(word1,word2,0,0,dp);
     }
 };
