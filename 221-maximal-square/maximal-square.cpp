f68c0bba12a333ea2bf151a255c0930277b45030
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-12 11:13:52 +0530

    Time: 4 ms (86.86%) | Memory: 30.9 MB (63.05%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -3,35 +3,17 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         int ans=0;
         for(int i=0;i<matrix.size();i++){
-            if(matrix[i][0]=='1'){
-                dp[i][0]=1;
-                ans=1;
+            for(int j=0;j<matrix[i].size();j++){
+                 if(matrix[i][j]=='1'){
+                    int val=min(dp[i][j],min(dp[i+1][j],dp[i][j+1]));
+                    dp[i+1][j+1]=1+val;
+                    ans=max(ans,dp[i+1][j+1]);
+                 }
             }
         }
-        for(int j=0;j<matrix[0].size();j++){
-            if(matrix[0][j]=='1'){
-                dp[0][j]=1;
-                ans=1;
-            }
-        }
-
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]=='1'){
-                    int val=dp[i-1][j-1];
-                    val=min(val,dp[i][j-1]);
-                    val=min(val,dp[i-1][j]);
-                    ans=max(ans,val+1);
-                    dp[i][j]=val+1;
-                    
-                }
-            }
-        }
-
-       
         return ans*ans;
     }
 };
