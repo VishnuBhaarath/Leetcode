873dec3e7c587ab868e59916284f6e5d8b13c972
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-13 11:35:19 +0530

    Time: 7 ms (72.32%) | Memory: 23 MB (11.82%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-    bool check(vector<int>& piles,int m,int h){
-        int cnt=0;
+    long long int check(int m,vector<int>& piles){
+        long long int days=0;
         for(int i=0;i<piles.size();i++){
             int q=piles[i]/m;
-            cnt+=q;
+            days+=q;
             if(piles[i]%m !=0){
-                cnt+=1;
-            }
-            if(cnt>h){
-                return false;
+                days+=1;
             }
         }
-        return true;
+        return days;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n=piles.size();
-        sort(piles.begin(),piles.end());
-        if(h==n){
-            return piles[n-1];
+        long long int r=piles[0];
+        for(int i=0;i<piles.size();i++){
+            if(piles[i]>r){
+                r=piles[i];
+            }
         }
-        int l=1;
-        int r=piles[n-1];
-        int idx=-1;
+        long long int ans=-1;
+        long long int l=1;
         while(l<=r){
-            int m=l+(r-l)/2;
-            if(check(piles,m,h)){
-                if(idx==-1){
-                    idx=m;
-                }
-                else{
-                    idx=min(idx,m);
-                }
+            long long int m=l+(r-l)/2;
+            long long int val=check(m,piles);
+           
+            if(val<=h){
+                ans=m;
                 r=m-1;
-            } 
+            }
             else{
+                
                 l=m+1;
             }
 
         }
-         return idx;
+        return ans;
     }
 };
