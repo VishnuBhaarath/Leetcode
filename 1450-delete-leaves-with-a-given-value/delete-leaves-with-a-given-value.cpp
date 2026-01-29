8a47e28cf50b3ae2e6f445eb04ebca20d87e131a
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-29 20:32:02 +0530

    Time: 11 ms (3.09%) | Memory: 23.3 MB (81.71%) - LeetSync

---
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* prev;
+    int l=0;
+    void traversal(TreeNode* root,int target,int t){
+        if(root==NULL){
+            return;
+        }
+       
+        if(root->val==target){
+            if(root->left==NULL && root->right==NULL){
+                if(t==0){
+                    prev->left=NULL;
+                }
+                if(t==1){
+                    prev->right=NULL;
+                }
+                 l=1;
+                return;
+
+            }
+          
+        }
+      
+        prev=root;
+        traversal(root->left,target,0);
+        prev=root;
+        traversal(root->right,target,1);
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        prev=root;
+        TreeNode* temp=root;
+        while(1){
+        root=temp; 
+        if(temp->val==target){
+            if(temp->left==NULL && temp->right==NULL){
+                return NULL;
+            }
+        }
+        l=0;
+        traversal(root,target,-1);
+        cout<<l;
+        cout<<"\n";
+        if(l==0){
+        return temp;
+        }
+        }
+        return temp;
+    }
+};
