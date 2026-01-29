7512ca1d0f88f3748f6e2cd9899951a3a41b0933
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-29 20:45:02 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (13.66%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -11,50 +11,16 @@
  */
 class Solution {
 public:
-    TreeNode* prev;
-    int l=0;
-    void traversal(TreeNode* root,int target,int t){
-        if(root==NULL){
-            return;
-        }
-       
-        if(root->val==target){
-            if(root->left==NULL && root->right==NULL){
-                if(t==0){
-                    prev->left=NULL;
-                }
-                if(t==1){
-                    prev->right=NULL;
-                }
-                 l=1;
-                return;
-
-            }
-          
-        }
-      
-        prev=root;
-        traversal(root->left,target,0);
-        prev=root;
-        traversal(root->right,target,1);
-    }
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        prev=root;
-        TreeNode* temp=root;
-        while(1){
-        root=temp; 
-        if(temp->val==target){
-            if(temp->left==NULL && temp->right==NULL){
-                return NULL;
-            }
+        if(root==NULL){
+            return NULL;
         }
-        l=0;
-        traversal(root,target,-1);
-        
-        if(l==0){
-        return temp;
+        root->left=removeLeafNodes(root->left,target);
+        root->right=removeLeafNodes(root->right,target);
+        if(root->left==NULL && root->right==NULL && root->val==target){
+            return NULL;
         }
-        }
-        return temp;
+        return root;
+
     }
 };
