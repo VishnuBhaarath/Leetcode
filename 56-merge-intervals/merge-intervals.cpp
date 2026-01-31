f0c3de685db5f33b36e28fb7cb43a2f668fe020e
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-31 12:46:05 +0530

    Time: 7 ms (40.69%) | Memory: 25.1 MB (7.16%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-
-        int l=intervals[0][0];
-        int r=intervals[0][1];
-        vector<vector<int>> ans;
-
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]>r){
-                ans.push_back({l,r});
-                l=intervals[i][0];
-                r=intervals[i][1];
-            }
-            else{
-                l=min(l,intervals[i][0]);
-                r=max(r,intervals[i][1]);
-            }
+        map<int,int> umap;
+        for(int i=0;i<intervals.size();i++){
+            umap[intervals[i][0]]++;
+            umap[intervals[i][1]]--;
         }
+        vector<vector<int>> ans;
+        int cnt=0;
+        vector<int> v;
 
-        ans.push_back({l,r});
+        for(auto x:umap){
+          if(v.size()==0){
+             v.push_back(x.first);
+          }
+          cnt+=x.second;
+          if(cnt==0){
+            v.push_back(x.first);
+            ans.push_back(v);
+            v.clear();
+          }
+        }
         return ans;
     }
 };
