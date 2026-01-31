aa85fbf300405625e953456c82b326e012b0f6e0
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-01-31 13:01:29 +0530

    Time: 3 ms (30.81%) | Memory: 23.7 MB (5.28%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> dp;
-        int start=newInterval[0];
-        int end=newInterval[1];
-        int n=intervals.size();
-        int t=0;
-        int i=0;
-        while(i<intervals.size() && intervals[i][1]<start){
-            dp.push_back({intervals[i][0],intervals[i][1]});
-            i+=1;
+        map<int,int> umap;
+        for(int i=0;i<intervals.size();i++){
+            umap[intervals[i][0]]+=1;
+            umap[intervals[i][1]]-=1;
         }
-        while(i<intervals.size() && intervals[i][0]<=end){
-            start=min(start,intervals[i][0]);
-            end=max(end,intervals[i][1]);
-            i+=1;
+      umap[newInterval[0]]+=1;
+umap[newInterval[1]]-=1;
+        vector<vector<int>> ans;
+        vector<int> v;
+        int cnt=0;
+        for(auto x:umap){
+             if(v.size()==0){
+                v.push_back(x.first);
+             }
+             cnt+=x.second;
+             if(cnt==0){
+                v.push_back(x.first);
+                ans.push_back(v);
+                v.clear();
+             }
         }
-        dp.push_back({start,end});
-
-         while(i<intervals.size() && intervals[i][0]>end){
-            dp.push_back({intervals[i][0],intervals[i][1]});
-            i+=1;
-         }
-        return dp;
+        return ans;
     }
 };
