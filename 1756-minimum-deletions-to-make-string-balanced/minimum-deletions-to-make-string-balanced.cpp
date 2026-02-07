49936035480e7c083a0c172ebc4c26a258b431e6
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-02-07 11:55:26 +0530

    Time: 25 ms (58.88%) | Memory: 25.6 MB (54.88%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n=s.size();
-        vector<int> v1(n,0);
-        vector<int> v2(n,0);
-        int cnt=0;
-        for(int i=n-1;i>=0;i--){
-             v1[i]=cnt;
-             if(s[i]=='a'){
-                cnt+=1;
-             }
-        }
-        cnt=0;
-         int ans=-1;
-        for(int i=0;i<n;i++){
-            int val=v1[i]+cnt;
-            if(ans==-1){
-                ans=val;
-            }
-            else{
-                ans=min(ans,val);
-            }
-            if(s[i]=='b'){
-                cnt+=1;
+         int res = 0, count = 0;
+        for (char c : s){
+            if (c == 'b')
+                count++;
+            else if (count){
+                res++;
+                count--;
             }
         }
-       
+        return res;
         
-        return ans;
     }
 };
