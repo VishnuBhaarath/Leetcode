3b3d53c54795a23ddb7be6730a723b12a40b28af
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-02-07 22:06:30 +0530

    Time: 31 ms (7.19%) | Memory: 10.9 MB (40.25%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        string s1=s;
         int ans=0;
-        int k1=k;
+        int temp=k;
         for(char ch='A';ch<='Z';ch++){
             int i=0;
             int j=0;
-            int cnt=0;
-           
-            k=k1;
-           
+            k=temp;
+         
             while(j<s.size()){
-              
                 if(s[j]==ch){
+                      int sz=j-i+1;
+                    ans=max(ans,sz);
+                    j+=1;
                   
-                    cnt+=1;
                 }
                 else{
                     if(k>0){
                         k-=1;
-                        cnt+=1;
+
+                         int sz=j-i+1;
+                    ans=max(ans,sz);
+                     j+=1;
+                
                     }
                     else{
                         while(s[i]==ch){
-                           cnt-=1;
-                           i+=1;
+                            i+=1;
                         }
                         i+=1;
-                        
+                       
+                        int sz=j-i+1;
+                        ans=max(ans,sz);
+                         j+=1;
                     }
                 }
-                j+=1;
-                ans=max(ans,cnt);
-                
+               
             }
-           
+          
         }
-
         return ans;
     }
 };
