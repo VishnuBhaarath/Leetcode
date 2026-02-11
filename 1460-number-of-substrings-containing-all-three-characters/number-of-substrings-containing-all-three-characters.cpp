dedc9a35989f38623c2da3076ccad1139b2959c1
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-02-11 11:36:14 +0530

    Time: 868 ms (5.30%) | Memory: 10.8 MB (94.96%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,18 +1,86 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        
-         vector<int> m(3,0);
-        int i=0,j=0,n=s.size(),ans=0;
-        while(j<n){
-            m[s[j]-'a']++;
-            while(m[0] && m[1] && m[2]){
-                ans+=n-j;
-                m[s[i]-'a']--;
-                i++;
-            }
-            j++;
+        int a=-1;
+        int b=-1;
+        int c=-1;
+        int cnt=0;
+        for(int i=0;i<s.size();i++){
+             if(s[i]=='a'){
+                 a=i;
+             }
+             if(s[i]=='b'){
+                b=i;
+             }
+             if(s[i]=='c'){
+                c=i;
+             }
+             if(a!=-1 && b!=-1 && c!=-1){
+                break;
+             }
         }
-        return ans;
+        if(a==-1 || b==-1 || c==-1){
+            return cnt;
+        }
+        for(int i=0;i<s.size();i++){
+             if(s[i]=='a'){
+                if(b==-1 || c==-1){
+                    return cnt;
+                }
+                int val=max(b,c);
+                cnt+=(s.size()-val);
+                if(a==i){
+                     for(int j=i+1;j<s.size();j++){
+                         if(s[j]=='a'){
+                            a=j;
+                            break;
+                         }
+                     }
+                     if(a==i){
+                        a=-1;
+                     }
+                }
+             }
+             else if(s[i]=='b'){
+                if(a==-1 || c==-1){
+                    return cnt;
+                }
+                int val=max(a,c);
+                cnt+=(s.size()-val);
+                if(b==i){
+                     for(int j=i+1;j<s.size();j++){
+                         if(s[j]=='b'){
+                            b=j;
+                            break;
+                         }
+                     }
+                     if(b==i){
+                        b=-1;
+                     }
+                }
+             }
+             else if(s[i]=='c'){
+                 if(a==-1 || b==-1){
+                    return cnt;
+                 }
+                 int val=max(a,b);
+                 cnt+=(s.size()-val);
+                 if(c==i){
+                     for(int j=i+1;j<s.size();j++){
+                         if(s[j]=='c'){
+                            c=j;
+                            break;
+                         }
+                     }
+                     if(c==i){
+                        c=-1;
+                     }
+                 }
+
+                 }
+             cout<<cnt;
+             cout<<"\n";
+        }
+        return cnt;
     }
 };
