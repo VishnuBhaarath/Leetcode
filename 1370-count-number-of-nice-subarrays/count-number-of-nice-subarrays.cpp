7ac5c85aea654bf8bf51f0f090c699a9647cca07
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-02-11 09:38:39 +0530

    Time: 207 ms (5.13%) | Memory: 110.7 MB (5.11%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& v, int goal) {
-         int n=v.size();
-         vector<int> nums;
-         for(int i=0;i<v.size();i++){
-            if(v[i]%2!=0){
-                nums.push_back(1);
-            }
-            else{
-                nums.push_back(0);
-            }
-         }
-        vector<int> psum;
-        psum.push_back(nums[0]);
+    int numberOfSubarrays(vector<int>& nums, int k) {
         map<int,int> umap;
-        for(int i=1;i<nums.size();i++){
-             int sum=psum[psum.size()-1]+nums[i];
-             psum.push_back(sum);
-        }
-        
         int cnt=0;
-        umap[0]=1;
-
-        for(int i=0;i<psum.size();i++){
-             int val=psum[i];
-            
-             cout<<umap[val-goal];
-           
-              cnt+=umap[val-goal];
-             umap[val]+=1;
+        umap[0]+=1;
+        int ans=0;
+        for(auto it:nums){
+            if((it%2)!=0){
+                cnt+=1;
+            }
+            ans+=umap[cnt-k];
+            umap[cnt]+=1;
 
         }
-       
- 
-        return cnt;
+        return ans;
     }
 };
