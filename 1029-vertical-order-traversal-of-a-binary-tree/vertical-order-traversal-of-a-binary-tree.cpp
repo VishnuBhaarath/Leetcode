d40dd84cfa39eebf1c5abd01c75d957ace2dfc2f
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-02-17 10:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (88.53%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,52 +6,53 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        queue<pair<TreeNode*, int>> q;
-        map<int,vector<int>> umap;
-        q.push({root, 0});
-        umap[0].push_back(root->val);
-        while (!q.empty()) {
-            int sz = q.size();
-            map<int,vector<int>> umap1;
-            while (sz > 0) {
-                pair<TreeNode*,int> p = q.front();
-                q.pop();
-                if (p.first->left != NULL) {
-                    q.push({p.first->left, p.second - 1});
-                    umap1[p.second-1].push_back(p.first->left->val);
-                  //  umap[p.second-1].push_back(p.first->left->val);
-                }
-                if (p.first->right != NULL) {
-                    q.push({p.first->right, p.second + 1});
-                      //umap[p.second+1].push_back(p.first->right->val);
-                       umap1[p.second+1].push_back(p.first->right->val);
-
-                }
-
-                sz -= 1;
-            }
-            for(auto x:umap1){
-                vector<int> v=x.second;
-                sort(v.begin(),v.end());
-                for(int i=0;i<v.size();i++){
-                    umap[x.first].push_back(v[i]);
-                }
-            }
-        }
+        map<int,vector<pair<int,int>>> umap; 
+        vector<vector<int>> ans;
+     
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
+              int row=0;
+        while(!q.empty()){
+            int sz=q.size();
+      
+            while(sz>0){
+             pair<TreeNode*,int> p=q.front();
+            
+             umap[p.second].push_back({row,p.first->val});
         
-        vector<vector<int>> dp;
-        for(auto x:umap){
-            dp.push_back(x.second);
+             q.pop();
+             if(p.first->left!=NULL){
+                q.push({p.first->left,p.second-1});
+             }
+             if(p.first->right!=NULL){
+                q.push({p.first->right,p.second+1});
+             }
+             sz-=1;
+             
+             }
+            row+=1;
         }
-       
+        cout<<"\n";
+        for(auto x:umap){
+             vector<pair<int,int>> v=x.second;
+             sort(v.begin(),v.end());
+         
+             vector<int> v1;
+             for(int j=0;j<v.size();j++){
+               
+                
+                 v1.push_back(v[j].second);
 
-        return dp;
+             }
+        
+             ans.push_back(v1);
+        }
+        return ans;
     }
 };
