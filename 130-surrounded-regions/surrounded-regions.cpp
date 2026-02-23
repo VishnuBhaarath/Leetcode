4616fb319d116dfd9a5bd3ba58e2b03567a7de56
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-02-23 10:43:58 +0530

    Time: 4 ms (14.95%) | Memory: 16.8 MB (10.15%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,38 +1,62 @@
 class Solution {
 public:
-    vector<int> delrow = {-1, 0, 1, 0}, delcol = {0, 1, 0, -1};
-    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<char>>& board) {
-        int n = board.size(), m = board[0].size();
-        vis[r][c] = 1;
-        for(int i = 0; i < 4; i++) {
-            int nrow = r + delrow[i], ncol = c + delcol[i]; 
-            if(nrow>=0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && board[nrow][ncol] == 'O') {
-                dfs(nrow, ncol, vis, board); 
+    void func(int i,int j,vector<vector<char>>& board,int n,int m,vector<vector<int>> &visited){
+        vector<int> r={1,-1,0,0};
+        vector<int> c={0,0,1,-1};
+        queue<pair<int,int>> q;
+       
+        q.push({i,j});
+        visited[i][j]=1;
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz>0){
+            pair<int,int> p=q.front();
+            q.pop();
+            int x=p.first;
+            int y=p.second;
+            for(int i=0;i<4;i++){
+                int x1=x+r[i];
+                int y1=y+c[i];
+                if((x1>=0 && x1<n) && (y1>=0 && y1<m)){
+                     if(board[x1][y1]=='O'){
+                         if(visited[x1][y1]==0){
+                             q.push({x1,y1});
+                             visited[x1][y1]=1;
+                         }
+                     }
+                }
             }
+            sz-=1;
+            }
+
         }
-        
     }
     void solve(vector<vector<char>>& board) {
-        int n = board.size(), m = board[0].size(); 
-        vector<vector<int>> vis(n, vector<int> (m, 0));
-        for(int i = 0; i < m; i++) {
-            // first row
-            if(!vis[0][i] && board[0][i] == 'O') dfs(0, i, vis, board);
-            //last row
-            if(!vis[n-1][i] && board[n-1][i] == 'O') dfs(n-1, i, vis, board);
+        int n=board.size();
+        int m=board[0].size();
+        vector<vector<int>> visited(n,vector<int> (m,0));
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O')
+{
+            func(i,0,board,n,m,visited);}
+            if(board[i][m-1]=='O'){
+            func(i,m-1,board,n,m,visited);}
         }
-
-        for(int i = 0; i < n; i++) {
-            // first col
-            if(!vis[i][0] && board[i][0] == 'O') dfs(i, 0, vis, board);
-            // last col
-            if(!vis[i][m-1] && board[i][m-1] == 'O') dfs(i, m-1, vis, board);
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O')
+            func(0,j,board,n,m,visited);
+            if(board[n-1][j]=='O'){
+            func(n-1,j,board,n,m,visited);}
+        
         }
-
-        for(int i = 0; i < n; i ++) {
-            for(int j = 0; j < m; j++) {
-                if(!vis[i][j] && board[i][j] == 'O') board[i][j] = 'X';
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                if(board[i][j]=='O'){
+                    if(visited[i][j]==0){
+                        board[i][j]='X';
+                    }
+                }
             }
-        } 
+        }
     }
 };
