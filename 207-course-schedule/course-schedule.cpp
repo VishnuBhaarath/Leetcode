29e5f1e970ce904304fb32672d9027e070a7e6b9
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-02-23 12:23:29 +0530

    Time: 8 ms (36.74%) | Memory: 20.6 MB (13.66%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-          vector<int> v(numCourses,0);
+        vector<int> v(numCourses,0);
+        map<int,vector<int>> umap;
         for(int i=0;i<prerequisites.size();i++){
-            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-             v[prerequisites[i][1]]+=1;
+            v[prerequisites[i][1]]+=1;
+            umap[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
 
-      
-        
         queue<int> q;
-        
         for(int i=0;i<v.size();i++){
             if(v[i]==0){
-            q.push(i);}
+                q.push(i);
+            }
         }
-        int cnt=0;
+        int cnt=q.size();
         while(!q.empty()){
-           
             int tp=q.front();
             q.pop();
-            cnt+=1;
-            for(int i=0;i<adj[tp].size();i++){
-                v[adj[tp][i]]-=1;
-                if(v[adj[tp][i]] == 0){
-                    q.push(adj[tp][i]);
+            vector<int> v1=umap[tp];
+            for(int j=0;j<v1.size();j++){
+                v[v1[j]]-=1;
+                if(v[v1[j]]==0){
+                    q.push(v1[j]);
+                    cnt+=1;
                 }
             }
-            
-           
-            
         }
         if(cnt==numCourses){
             return true;
