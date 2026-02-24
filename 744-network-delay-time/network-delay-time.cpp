8ad26cc0cc97ffbdb5a9dfb88446d5a8fa0359a0
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-02-24 11:43:46 +0530

    Time: 198 ms (5.05%) | Memory: 49.5 MB (6.75%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        map<int,vector<pair<int,int>>> umap;
+        vector<int> v(n+1,INT_MAX);
+        vector<vector<int>> adj(n+1);
+
         for(int i=0;i<times.size();i++){
-            umap[times[i][0]].push_back({times[i][1],times[i][2]});
+            adj[times[i][0]].push_back(times[i][1]);
         }
-   priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
-   vector<int> ans(n+1,INT_MAX);
-   
-   ans[k]=0;
-   pq.push({0,k});
-   while(!pq.empty()){
-      pair<int,int> tp=pq.top();
-      pq.pop();
-      vector<pair<int,int>> v=umap[tp.second];
-      
-      for(int i=0;i<v.size();i++){
-          if(ans[tp.second]+v[i].second < ans[v[i].first]){
-            ans[v[i].first]=ans[tp.second]+v[i].second;
-            pq.push({ans[v[i].first],v[i].first});
-          }
-      }
+        map<pair<int,int>,int> umap;
+        for(int i=0;i<times.size();i++){
+            umap[{times[i][0],times[i][1]}]=times[i][2];
+        }
+        priority_queue<pair<int,int>> pq;
+        pq.push({0,k});
+        v[k]=0;
+        while(!pq.empty()){
+            pair<int,int> p=pq.top();
+            pq.pop();
+            int idx=p.second;
+            int cost=p.first;
+           
+            for(int i=0;i<adj[idx].size();i++){
+              
+               if(umap[{idx,adj[idx][i]}]+cost < v[adj[idx][i]]){
+                 v[adj[idx][i]]=umap[{idx,adj[idx][i]}]+cost;
+                 pq.push({v[adj[idx][i]],adj[idx][i]});
+               
+               }
+              
+            }
+           
+        }
+        int c=0;
+        for(int i=1;i<v.size();i++){
+            if(v[i]==INT_MAX){
+                return -1;
+            }
+            c=max(c,v[i]);
 
-   }
-   int val=ans[1];
-   if(val==INT_MAX){
-    return -1;
-   }
-   for(int i=2;i<ans.size();i++){
-    if(ans[i]==INT_MAX){
-        return -1;
-    }
-    if(ans[i]>val){
-        val=ans[i];
-    }
-   
-   }
+        }
 
-     return val;
 
+        return c;
     }
 };
