54a613e2400821cfbc7e143632a30934d7813b8a
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-01 12:30:30 +0530

    Time: 3 ms (7.32%) | Memory: 10.8 MB (38.09%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -3,29 +3,46 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,0);
-        if(n==1){
-            return nums[0];
-        }
-        dp[1]=nums[1];
-        if(n>2){
-        dp[2]=max(nums[2],dp[1]);
-        for(int i=3;i<nums.size();i++){
-            int val1=nums[i]+dp[i-2];
-            int val2=dp[i-1];
-            dp[i]=max(val1,val2);
-        }
-        }
-        int ans=dp[n-1];
-       
-        dp[0]=nums[0];
-        dp[1]=max(dp[0],nums[1]);
-        for(int i=2;i<(n-1);i++){
-            int val1=nums[i]+dp[i-2];
-            int val2=dp[i-1];
-            dp[i]=max(val1,val2);
-        }
         
-        ans=max(ans,dp[n-2]);
+        dp[0]=nums[0];
+        int ans=dp[0];
+        if(n==1){
+            return ans;
+        }
+        dp[1]=max(dp[0],nums[1]);
+        ans=max(ans,dp[1]);
+        for(int i=2;i<nums.size()-1;i++){
+             dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+             ans=max(ans,dp[i]);
+        }
+        for(int i=0;i<dp.size();i++){
+            cout<<dp[i];
+            cout<<" ";
+        }
+        cout<<"\n";
+
+        vector<int> dp1(n,0);
+        
+        dp1[1]=nums[1];
+        ans=max(ans,dp1[1]);
+        if(n==2){
+            return ans;
+        }
+        dp1[2]=max(dp1[1],nums[2]);
+        ans=max(ans,dp1[2]);
+        for(int i=3;i<nums.size();i++){
+
+             dp1[i]=max(dp1[i-1],dp1[i-2]+nums[i]);
+             ans=max(ans,dp1[i]);
+        }
+
+
+          for(int i=0;i<dp1.size();i++){
+            cout<<dp1[i];
+            cout<<" ";
+        }
+        cout<<"\n";
         return ans;
+        
     }
 };
