d03ed5c211cb65491a436225e0ab566e08e655b2
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-01 10:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -9,20 +9,11 @@ public:
         if(n==1){
             return ans;
         }
-        dp[1]=nums[1];
+        dp[1]=max(dp[0],nums[1]);
         ans=max(ans,dp[1]);
         for(int i=2;i<nums.size();i++){
-             dp[i]=dp[i-1];
-            
-             for(int j=i-2;j>=0;j--){
-                 dp[i]=max(dp[i],nums[i]+dp[j]);
-                 ans=max(ans,dp[i]);
-             }
-        }
-
-        for(int i=0;i<dp.size();i++){
-            cout<<dp[i];
-            cout<<" ";
+             dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+             ans=max(ans,dp[i]);
         }
         return ans;
     }
