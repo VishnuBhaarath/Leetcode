54f87d5a9d42f4b259edf6abbddc0fe0d8311fbc
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-10 11:26:22 +0530

    Time: 25 ms (9.13%) | Memory: 12.5 MB (31.09%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
-    int func1(int i,int j,vector<int>&arr){
-        int val=arr[i];
-        for(int k=i;k<=j;k++){
-            val=max(val,arr[k]);
-        }
-       
-        return (j-i+1)*val;
-    }
-    int func(vector<int>&arr,int i,int k,int n,vector<int>& dp){
-        if(i>=n){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int ans=0;
-        int val=0;
-        for(int j=i;j<min(i+k,n);j++){
-            val=max(val,arr[j]);
-            int sum=(j-i+1)*val+func(arr,j+1,k,n,dp);
-            ans=max(ans,sum);
-           
-            
-        }
-        return dp[i]=ans;
-    }
+   int ans=0;
+   int check(vector<int>& arr, int i, int j){
+    
+     int val=arr[i];
+     for(int k=i;k<j;k++){
+        val=max(val,arr[k]);
+     }
+     return (j-i)*val;
+
+   }
+   void func(vector<int>& arr, int k,int i,int sum){
+      if(i==arr.size()){
+       ans=max(ans,sum);
+        return;
+      }
+      int n=arr.size();
+      int val1=-1;
+      for(int j=i+1;j<min(n+1,i+k+1);j++){
+        
+           val1=max(val1,arr[j-1]);
+           int val2=val1*(j-i);
+          
+         // int val=check(arr,i,j);
+         func(arr,k,j,sum+val2);
+      }
+   }
+
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<int> dp(n,0);
-        for(int i=n-1;i>=0;i--){
-            int val=0;
-            int ans=0;
-            for(int j=i;j<min(i+k,n);j++){
-               val=max(val,arr[j]);
-               int sum=(j-i+1)*val;
-               if((j+1)<n){
-               sum+=dp[j+1];}
-                ans=max(ans,sum);
-            }
-            dp[i]=ans;
-        }
-        //return func(arr,0,k,n,dp);
-        return dp[0];
+       // vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
+       // func(arr,k,0,0);
+       vector<int> dp(n+1,0);
+       dp[n-1]=arr[n-1];
+       for(int i=n-2;i>=0;i--){
+          int val=0;
+          int ans=0;
+         for(int j=i;j<min(n,i+k);j++){
+             val=max(val,arr[j]);
+             int sum=(j-i+1)*val;
+             int temp=0;
+             if(j+1<n){
+                temp+=dp[j+1];
+             }
+             ans=max(ans,sum+temp);
+             dp[i]=max(dp[i],ans);
 
+         }
+       }
+       for(int i=0;i<dp.size();i++){
+        cout<<dp[i];
+        cout<<" ";
+       }
+       cout<<"\n";
+        return dp[0];
     }
 };
