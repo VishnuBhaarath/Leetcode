2a5a29fe91b1d6e9527d4ccd0e4d0bdb56bffba0
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-11 11:27:09 +0530

    Time: 23 ms (68.26%) | Memory: 17.9 MB (71.34%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(),coins.end());
+        vector<int> dp(amount+1,0);
+        for(int i=0;i<coins.size();i++){
+             if(coins[i]<=amount){
+                dp[coins[i]]=1;
+                if(coins[i]==amount){
+                    return 1;
+                }
+             }
+        }
         if(amount==0){
             return 0;
         }
-        vector<int> dp(amount+1,0);
-        for(int i=0;i<coins.size();i++){
-            if(coins[i]<dp.size()){
-                dp[coins[i]]=1;
-            }
-        }
-        for(int i=1;i<dp.size();i++){
-            for(int j=0;j<coins.size();j++){
-                if(dp[i]>0){
-                    if((long long)(i)+(long long)(coins[j]) <= (long long)(amount)){
-                        if(dp[i+coins[j]]==0){
-                            dp[i+coins[j]]=dp[i]+1;
-                        }
-                        else{
-                            dp[i+coins[j]]=min(dp[i+coins[j]],dp[i]+1);
-                        }
-                    }
+         for(int i=1;i<dp.size();i++){
+          
+             if(dp[i]!=0){
+                for(int j=0;j<coins.size();j++){
+                      if(((long long int)(i)+(long long int)(coins[j]))<=(long long int) amount){
+                         if(dp[i+coins[j]]==0){
+                             dp[i+coins[j]]=dp[i]+1;
+                         }
+                         else{
+                            dp[i+coins[j]]=min(dp[i]+1,dp[i+coins[j]]);
+                         }
+                      }
                 }
-            }
-        }
+             }
+            
+         }
        
-        if(dp[amount]==0){
-            return -1;
-        }
-        return dp[amount];
+
+       if(dp[amount]==0){
+        return -1;
+       }
+
+       return dp[amount];
     }
 };
