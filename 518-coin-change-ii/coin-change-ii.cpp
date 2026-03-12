4f258487959eff345a34b952784b2c68d8b40fb5
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-12 13:03:21 +0530

    Time: 51 ms (16.59%) | Memory: 61.9 MB (7.03%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
-    
-     
-
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
         if(amount==0){
             return 1;
         }
-        vector<vector<unsigned long long int>> dp(n+1,vector<unsigned long long int>(amount+1,0));
-        for(int i=n-1;i>=0;i--){
-            if(coins[i]<=amount){
-            dp[i][coins[i]]=1;}
-            for(int j=0;j<=amount;j++){
-
-               dp[i][j]+=dp[i+1][j];
-               if((j-coins[i])>=0){
-                dp[i][j]+= dp[i][j-coins[i]];
-               }
-               
-            }
-        }
+        sort(coins.begin(),coins.end());
+        unsigned long long n=coins.size();
+        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(amount+1,0));
         
-       
-       
-        return dp[0][amount];
+        for(long long int i=0;i<coins.size();i++){
+           for(long long int j=0;j<=amount;j++){
+                if(j<coins[i]){
+                    dp[i+1][j]=dp[i][j];
+                }
+                else if(j==coins[i]){
+                    dp[i+1][j]=dp[i][j]+1;
+                }
+                else{
+                    if(i==0){
+                       
+                        if(j==(unsigned long long int)(coins[i])){
+                            cout<<"in";
+                            dp[i+1][j]=1;
+                        }
+                        else{
+ dp[i+1][j]=dp[i][j]+dp[i+1][j-coins[i]];
+                        }
+                       
+                    }
+                    else{
+                       
+                        dp[i+1][j] = dp[i][j] + dp[i+1][j - coins[i]];}
+                        
+                    }
+                
+                   
+                }
+           }
+        
+        
+        return dp[n][amount];
     }
 };
