73324392ff18f98610276b1120d3d46e4d2e2f56
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-13 11:15:02 +0530

    Time: 1 ms (46.51%) | Memory: 16.2 MB (35.11%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -20,9 +20,20 @@ public:
         int n=prices.size();
         int i=0;
         int k=0;
-        dp.resize(n,vector<int>(2,-1));
-      
+        dp.resize(n+2,vector<int>(2,0));
 
-        return func(i,k,prices);
+         for (int i = n - 1; i >= 0; i--) {
+        // Step 4: Copy recurrence exactly, func(...) → dp[...]
+        
+        // k=0 means: currently NOT holding stock
+        dp[i][0] = max(-prices[i] + dp[i+1][1],  // buy
+                        dp[i+1][0]);               // skip
+        
+        // k=1 means: currently HOLDING stock
+        dp[i][1] = max(+prices[i] + dp[i+2][0],  // sell (cooldown=i+2!)
+                        dp[i+1][1]);               // skip
+    }
+
+        return dp[0][0]; //func(i,k,prices);
     }
 };
