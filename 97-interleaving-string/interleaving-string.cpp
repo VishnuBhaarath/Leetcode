fac7990828872d0ef1e2e21b99e642f646f7a155
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-16 11:30:15 +0530

    Time: 3 ms (56.61%) | Memory: 9.7 MB (39.58%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -46,13 +46,7 @@ public:
             }
         }
 
-        for(int i=0;i<dp.size();i++){
-            for(int j=0;j<dp[0].size();j++){
-                cout<<dp[i][j];
-                cout<<" ";
-            }
-            cout<<"\n";
-        }
+       
         return dp[n][m];
     }
 };
