c23564c534963817a1fdd4a2843f5c814751d392
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-17 12:49:27 +0530

    Time: 286 ms (5.43%) | Memory: 20.7 MB (74.53%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,44 +1,49 @@
 class Solution {
 public:
+    vector<int> r={1,-1,0,0};
+    vector<int> c={0,0,1,-1};
+    vector<vector<int>> dp;
+    int traversal(int i,int j,vector<vector<int>>& matrix,int d){
+        int n=matrix.size();
+        int m=matrix[0].size();
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        for(int k=0;k<4;k++){
+            int x1=i+r[k];
+            int y1=j+c[k];
+            if((x1>=0 && x1<n) && (y1>=0 && y1<m)){
+                 
+                 if(matrix[x1][y1]>matrix[i][j]){
+                    dp[i][j]=max(dp[i][j],1+traversal(x1,y1,matrix,d+1));
+                 }
+            }
+
+        }
+     
+        if(dp[i][j]==-1){
+            dp[i][j]=1;
+        }
+        return dp[i][j];
+    }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int> r={1,-1,0,0};
-        vector<int> c={0,0,1,-1};
-        int ans=1;
-          vector<vector<int>> dp(n,vector<int>(m,0));
+        dp.resize(n,vector<int>(m,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                 queue<pair<pair<int,int>,int>> q;
-
-                 q.push({{i,j},1});
-              //   dp[i][j]=1;
-                 while(!q.empty()){
-                
-                     pair<int,int> p=q.front().first;
-                     int d=q.front().second;
-                     ans=max(ans,d);
-                     q.pop();
-                     int x=p.first;
-                     int y=p.second;
-                     dp[x][y]=max(dp[x][y],d);
-                     for(int k=0;k<4;k++){
-                        int x1=x+r[k];
-                        int y1=y+c[k];
-                        if(x1>=0 && x1<n){
-                             if(y1>=0 && y1<m){
-                                 if(matrix[x1][y1]>matrix[x][y] && dp[x1][y1]< d+1){
-                                    q.push({{x1,y1},d+1});
-                                    dp[x1][y1]=d+1;
-                                 }
-                             }
-                        }
-                     }
-                 
-                     }
-                 }
-        
+                traversal(i,j,matrix,0);
+            }
         }
-return ans;
+        int ans=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<dp[i][j];
+                ans=max(ans,dp[i][j]);
+                cout<<" ";
+            }
+            cout<<"\n";
+        }
+        return ans;
     }
 };
