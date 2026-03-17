8843f266704c2e8edac78a43b0534a49ea67cc57
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-17 12:50:55 +0530

    Time: 3 ms (95.40%) | Memory: 20.8 MB (74.53%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> r={1,-1,0,0};
     vector<int> c={0,0,1,-1};
     vector<vector<int>> dp;
+    int ans=1;
     int traversal(int i,int j,vector<vector<int>>& matrix,int d){
         int n=matrix.size();
         int m=matrix[0].size();
@@ -12,10 +13,11 @@ public:
         for(int k=0;k<4;k++){
             int x1=i+r[k];
             int y1=j+c[k];
-            if((x1>=0 && x1<n) && (y1>=0 && y1<m)){
-                 
+            if((x1>=0 && x1<n) && (y1>=0 && y1<m) ){
+                
                  if(matrix[x1][y1]>matrix[i][j]){
                     dp[i][j]=max(dp[i][j],1+traversal(x1,y1,matrix,d+1));
+                    ans=max(ans,dp[i][j]);
                  }
             }
 
@@ -35,15 +37,7 @@ public:
                 traversal(i,j,matrix,0);
             }
         }
-        int ans=1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                cout<<dp[i][j];
-                ans=max(ans,dp[i][j]);
-                cout<<" ";
-            }
-            cout<<"\n";
-        }
+      
         return ans;
     }
 };
