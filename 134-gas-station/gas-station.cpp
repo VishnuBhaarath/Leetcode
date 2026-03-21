e4d4d0aa318d51ca4a655168c4ca5aed58a95f9c
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-21 10:54:47 +0530

    Time: 14 ms (5.02%) | Memory: 126.5 MB (5.22%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int sum1=0;
-        for(int i=0;i<gas.size();i++){
-            gas[i]=gas[i]-cost[i];
-            sum1+=gas[i];
-        }
-       
-        if(sum1<0){
-            return -1;
-        }
-        int idx=-1;
-        int sum=0;
-    
-        for(int i=0;i<gas.size();i++){
-          
-             if(gas[i]>=0){
-                if(sum<0){
-                idx=i;
-                sum=gas[i];}
-                else{
-                    sum+=gas[i];
-                    if(idx==-1){
-                        idx=i;
-                    }
-                }
-             }
-             else{
-                sum+=gas[i];
-                if(sum<0){
-                    idx=-1;
-                }
-             }
-        }
-        return idx;
+            vector<int> v=gas;
+            vector<int> c=cost;
+
+            for(int i=0;i<gas.size();i++){
+                 v.push_back(gas[i]);
+                 c.push_back(cost[i]);
+            }
+            
+           
+
+            int sum=v[0];
+            int cnt=1;
+            int idx=0;
+
+            int i=1;
+            while(i<v.size()){
+                 if(sum>=c[i-1]){
+                     sum+=v[i];
+                     sum-=c[i-1];
+                     cnt+=1;
+                     if(cnt==gas.size()+1){
+                        return idx;
+                     }
+                 }
+                 else{
+                      sum=v[i];
+                      idx=i;
+                      cnt=1;
+                 }
+                 i+=1;
+            }
+
+
+          return  -1;
     }
 };
