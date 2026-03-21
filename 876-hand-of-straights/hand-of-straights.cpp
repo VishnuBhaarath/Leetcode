118e03efc48352e30565c8b7cfcd4e86dae68c5a
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-21 11:31:15 +0530

    Time: 25 ms (87.40%) | Memory: 31.1 MB (81.84%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        map<int, int> umap;
-        for (int i = 0; i < hand.size(); i++) {
-            umap[hand[i]] += 1;
+        sort(hand.begin(),hand.end());
+        unordered_map<int,int> umap;
+        for(int i=0;i<hand.size();i++){
+            umap[hand[i]]+=1;
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq;
-        for (auto x : umap) {
-            pq.push({x.first, x.second});
-        }
-        while (!pq.empty()) {
-            int sz = 0;
-            int prev = -1;
-            priority_queue<pair<int, int>, vector<pair<int, int>>,
-                           greater<pair<int, int>>>
-                pq1;
-            while (!pq.empty()) {
-                pair<int, int> p = pq.top();
-                pq.pop();
+        for(int i=0;i<hand.size();i++){
+           
+             if(umap[hand[i]]>0){
+                umap[hand[i]]-=1;
+                int j=hand[i]+1;
+                
+                int k=1;
+                while(k<groupSize){
+                    if(umap[j]>0){
+                        umap[j]-=1;
 
-                p.second -= 1;
-                if (sz > 0) {
-                    if (prev + 1 != p.first) {
+                    }
+                    else{
                         return false;
                     }
+                    j+=1;
+                    k+=1;
                 }
-                sz += 1;
-                prev = p.first;
-
-                if (p.second > 0)
-                    pq1.push(p);
-                if (sz == groupSize) {
-                    break;
-                }
-            }
-
-            if (sz != groupSize) {
-                return false;
-            }
-            while (!pq1.empty()) {
-                pq.push(pq1.top());
-                pq1.pop();
-            }
+             }
+           
         }
         return true;
     }
