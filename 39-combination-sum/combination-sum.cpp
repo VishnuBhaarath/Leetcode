75634f4ef0a07d024ebf3b14d98789f763a01475
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-24 11:15:24 +0530

    Time: 6 ms (27.20%) | Memory: 16.6 MB (28.38%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    set<vector<int>> s;
-    void func(vector<int>& candidates,vector<int>& v, int i, int target,int sum){
-        
-        if(sum>target){
-            return;
-        }
-        if(sum==target){
-             
-                ans.push_back(v);
-             
-             return;
-        }
-        if(i==candidates.size()){
-            return;
-        }
-        v.push_back(candidates[i]);
-        func(candidates,v,i,target,sum+candidates[i]);
-        v.pop_back();
-        func(candidates,v,i+1,target,sum);
-         
+vector<vector<int>> res;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        dfs(0, {}, 0, nums, target);
+        return res;
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> v;
-        func(candidates,v,0,target,0);
-        return ans;
-        
+      void dfs(int i, vector<int> cur, int total, vector<int>& nums, int target) {
+        if (total == target) {
+            res.push_back(cur);
+            return;
+        }
+
+        for (int j = i; j < nums.size(); j++) {
+            if (total + nums[j] > target) {
+                return;
+            }
+            cur.push_back(nums[j]);
+            dfs(j, cur, total + nums[j], nums, target);
+            cur.pop_back();
+        }
     }
 };
