d525d12dcdf7a38497563cb427797f8210197a1d
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-24 12:24:00 +0530

    Time: 4 ms (11.62%) | Memory: 10.7 MB (38.72%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    void func(vector<int>& nums,vector<int>& visited,vector<int> &v,vector<vector<int>>&ans){
-        
-        for(int i=0;i<nums.size();i++){
-            if(visited[i]==0){
-                visited[i]=1;
-                v.push_back(i);
-                func(nums,visited,v,ans);
+ vector<vector<int>> ans;
+    void func(vector<int>& nums,vector<int> &v,int i,vector<int> &visited){
+           if(i==nums.size()){
+           
+            return;
+         }
+         
+         if(v.size()==nums.size()){
+            
+         ans.push_back(v);
+            return;
+         }
+      
+         
+      
+        for(int j=0;j<nums.size();j++){
+            if(visited[j]==0){
+                 visited[j]=1;
+                 v.push_back(nums[j]);
+                 func(nums,v,i,visited);
+                 visited[j]=0;
+                 v.pop_back();
+               
             }
         }
-        int n=v.size();
-        if(n>0){
-        if(n==nums.size()){
-            vector<int> v1;
-            for(int i=0;i<n;i++){
-                v1.push_back(nums[v[i]]);
-            }
-            ans.push_back(v1);
-        }   
-        int val=v[n-1];
-        visited[val]=0;
-        v.pop_back();}
-        return;
+
     }
     vector<vector<int>> permute(vector<int>& nums) {
         int n=nums.size();
-        vector<int> visited(n,0);
         vector<int> v;
-         vector<vector<int>> ans;
-        func(nums,visited,v,ans);
+        vector<int> visited(n,0);
+        func(nums,v,0,visited);
        
-
         return ans;
     }
 };
