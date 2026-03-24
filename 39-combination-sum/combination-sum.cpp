46023fa42b9ec019f8af8011a7704748f213a80f
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-24 11:02:12 +0530

    Time: 3 ms (52.18%) | Memory: 14.8 MB (30.90%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    set<vector<int>> s;
+    void func(vector<int>& candidates,vector<int>& v, int i, int target,int sum){
+        
+        if(sum>target){
+            return;
+        }
+        if(sum==target){
+             if(s.find(v)==s.end()){
+                ans.push_back(v);
+                s.insert(v);
+             }
+             return;
+        }
+        if(i==candidates.size()){
+            return;
+        }
+        v.push_back(candidates[i]);
+        func(candidates,v,i,target,sum+candidates[i]);
+        v.pop_back();
+        func(candidates,v,i+1,target,sum);
+         
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> v;
+        func(candidates,v,0,target,0);
+        return ans;
+        
+    }
+};
