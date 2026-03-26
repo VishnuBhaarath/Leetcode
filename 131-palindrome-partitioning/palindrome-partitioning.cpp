334edccf028e5018a65e197921b6cb2e6b8c59b8
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-26 13:15:23 +0530

    Time: 165 ms (5.80%) | Memory: 171.6 MB (5.00%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    map<string,int> umap;
-    bool ischeck(string s){
+         vector<vector<int>> dp;
+    bool check(string s){
         int i=0;
         int j=s.size()-1;
         while(i<=j){
@@ -25,28 +25,29 @@ public:
         string s1="";
         for(int j=i;j<s.size();j++){
            s1+=s[j];
-           if(umap[s1]==0){
-              if(ischeck(s1)){
-                 v.push_back(s1);
-           func(j+1,s,v);
-           v.pop_back();
-           umap[s1]=1;
-              }
-              else{
-                 umap[s1]=-1;
-              }
-           }
-           else if(umap[s1]==1){
-             v.push_back(s1);
+           if(dp[i][j]==1){
+ v.push_back(s1);
            func(j+1,s,v);
            v.pop_back();
            }
-           
+          
           
         }
     }
     vector<vector<string>> partition(string s) {
         vector<string> v;
+        int n=s.size();
+    dp.resize(n,vector<int>(n,0));
+
+         for(int i=0;i<s.size();i++){
+            string st="";
+            for(int j=i;j<s.size();j++){
+                st+=s[j];
+                if(check(st)){
+                    dp[i][j]=1;
+                }
+            }
+         }
          func(0,s,v);
          return ans;
     }
