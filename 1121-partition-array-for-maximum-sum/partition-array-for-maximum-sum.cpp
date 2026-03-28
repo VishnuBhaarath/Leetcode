f1ce8a3b0f75e53488ea41a85a6a7c9bb5329e57
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-28 12:33:42 +0530

    Time: 7 ms (52.43%) | Memory: 12.5 MB (59.05%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,44 +1,13 @@
 class Solution {
 public:
-    int ans=INT_MIN;
-    vector<int> dp;
-    int func(vector<int>& arr,int i,int k,int n){
-        
-        if(i==n){
-           
-            
-            return 0;
-        }
-       if(dp[i]!=-1){
-        return dp[i];
-       }
-       int val=-1;
-       int ans=-1;
-       for(int j=i;j< min(n,(i+k));j++){
-          
-          val=max(val,arr[j]);
-          int sz=(j-i+1);
-          int val1=sz*val;
-          int sum= val1+func(arr,j+1,k,n);
-          ans=max(ans,sum);
-       //   sum+=(val*sz);
-          
-         
-         // sum-=(val*sz);
-       }
-return dp[i]=ans;
-         
-    }
+  
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
+        vector<int> dp;
         dp.resize(n+1,0);
 
    //   func(arr,0,k,n);
-        for(int i=0;i<dp.size();i++){
-            cout<<dp[i];
-            cout<<" ";
-        }
-        cout<<"\n";
+       
 
         for(int i=n-1;i>=0;i--){
             int val=-1;
@@ -53,11 +22,7 @@ return dp[i]=ans;
             }
             dp[i]=ans;
         }
-           for(int i=0;i<dp.size();i++){
-            cout<<dp[i];
-            cout<<" ";
-        }
-        cout<<"\n";
+         
       return dp[0];
     }
 };
