1e5e9a25764e774ebc860c6f9e3c36f68e162670
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-03-28 12:33:02 +0530

    Time: 51 ms (7.66%) | Memory: 12.4 MB (59.05%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,27 +1,63 @@
 class Solution {
 public:
-  
+    int ans=INT_MIN;
+    vector<int> dp;
+    int func(vector<int>& arr,int i,int k,int n){
+        
+        if(i==n){
+           
+            
+            return 0;
+        }
+       if(dp[i]!=-1){
+        return dp[i];
+       }
+       int val=-1;
+       int ans=-1;
+       for(int j=i;j< min(n,(i+k));j++){
+          
+          val=max(val,arr[j]);
+          int sz=(j-i+1);
+          int val1=sz*val;
+          int sum= val1+func(arr,j+1,k,n);
+          ans=max(ans,sum);
+       //   sum+=(val*sz);
+          
+         
+         // sum-=(val*sz);
+       }
+return dp[i]=ans;
+         
+    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-     
-       vector<int> dp(n+1,0);
-       dp[n-1]=arr[n-1];
-       for(int i=n-2;i>=0;i--){
-          int val=0;
-          int ans=0;
-         for(int j=i;j<min(n,i+k);j++){
-             val=max(val,arr[j]);
-             int sum=(j-i+1)*val;
-             int temp=0;
-             if(j+1<n){
-                temp+=dp[j+1];
-             }
-             ans=max(ans,sum+temp);
-             dp[i]=max(dp[i],ans);
+        dp.resize(n+1,0);
 
-         }
-       }
-      
-        return dp[0];
+   //   func(arr,0,k,n);
+        for(int i=0;i<dp.size();i++){
+            cout<<dp[i];
+            cout<<" ";
+        }
+        cout<<"\n";
+
+        for(int i=n-1;i>=0;i--){
+            int val=-1;
+            int ans=-1;
+            for(int j=i;j<min(n,i+k);j++){
+ val=max(val,arr[j]);
+          int sz=(j-i+1);
+          int val1=sz*val;
+          int sum=val1+dp[j+1];
+          ans=max(ans,sum);
+
+            }
+            dp[i]=ans;
+        }
+           for(int i=0;i<dp.size();i++){
+            cout<<dp[i];
+            cout<<" ";
+        }
+        cout<<"\n";
+      return dp[0];
     }
 };
