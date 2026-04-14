39c2952e596cfc2fb377caeb908ebd640b7c5a20
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-04-14 09:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (52.50%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -5,33 +5,21 @@ public:
         vector<int> v(n,1);
         for(int i=1;i<ratings.size();i++){
              if(ratings[i]>ratings[i-1]){
-                v[i]=v[i-1]+1;
+                 v[i]=v[i-1]+1;
              }
         }
-       
-        for(int i=n-1;i>0;i--){
-            if(ratings[i-1]>ratings[i]){
-                if(v[i-1]<=v[i]){
-                    v[i-1]=1+v[i];
-                }
-                if((i-2)>=0){
-                    if(ratings[i-2]>ratings[i-1]){
-                        if(v[i-2]<=v[i-1]){
-                            v[i-2]=v[i-1]+1;
-                        }
-                    }
-                }
-            }
+
+        for(int i=n-2;i>=0;i--){
+             if(ratings[i]>ratings[i+1]){
+                 if(v[i]<=v[i+1]){
+                    v[i]=v[i+1]+1;
+                 }
+             }
         }
         int sum=0;
-
         for(int i=0;i<v.size();i++){
-           
             sum+=v[i];
         }
-        
-   
-
         return sum;
     }
 };
