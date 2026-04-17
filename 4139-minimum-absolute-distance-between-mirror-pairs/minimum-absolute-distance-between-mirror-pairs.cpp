4b2c0c6323c72aa6a5b46eafe9341e4f6a245fbc
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-04-17 21:55:05 +0530

    Time: 484 ms (9.07%) | Memory: 165.9 MB (15.93%) - LeetSync

---
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minMirrorPairDistance(vector<int>& nums) {
+        map<int,int> umap;
+int ans=-1;
+        for(int i=0;i<nums.size();i++){
+          
+            if(umap[nums[i]]!=0){
+                int idx=umap[nums[i]];
+                int val=i+1-idx;
+                if(ans==-1){
+                    ans=val;
+                }
+                else{
+                    ans=min(ans,val);
+                                    }
+
+            }
+
+            while(nums[i]%10==0){
+                nums[i]=nums[i]/10;
+            }
+          //  umap[nums[i]]=i+1;
+            string st=to_string(nums[i]);
+            reverse(st.begin(),st.end());
+            int temp1=stoi(st);
+            umap[temp1]=i+1;
+          
+        }
+          return ans;
+    }
+
+};
