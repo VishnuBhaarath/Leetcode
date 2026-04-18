6dbdc1a0ba587cb596dbffae81b7a49d406d00f8
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-04-18 12:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (18.92%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int mirrorDistance(int n) {
+        int m=n;
+        while(n%10==0){
+            n=n/10;
+        }
+      
         string st=to_string(n);
         reverse(st.begin(),st.end());
-        int m=stoi(st);
-        return abs(n-m);
-        cout<<st;
-        return 27;
+        int val=stoi(st);
+
+        return abs(m-val);
     }
+
 };
