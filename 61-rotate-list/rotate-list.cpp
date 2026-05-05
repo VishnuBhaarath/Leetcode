0de0ded27a0957846616485036e91c1c91980294
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-05 10:45:15 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (63.05%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -11,51 +11,40 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        ListNode* curr=head;
-        ListNode* fast=head;
-        ListNode* slow=head;
-        if(k==0){
-            return head;
-        }
-        ListNode* curr1=head;
-        int cnt=0;
-        while(curr1!=NULL){
-            curr1=curr1->next;
-            cnt+=1;
-        }
-        if(cnt==0){
-            return head;
-        }
-        if(k>=cnt){
-        k=(k%cnt);}
+        ListNode* temp=head;
         
-        for(int i=0;i<k;i++){
-            fast=fast->next;
-            if(fast==NULL){
-                fast=head;
-            }
+        int n=0;
+        while(temp!=NULL){
+            n+=1;
+            temp=temp->next;
         }
-     
-        if(fast->next==NULL){
-            fast->next=curr;
-            head=slow->next;
-            slow->next=NULL;
+        if(n==0){
+            return NULL;
+        }
+        int q=k%n;
+        if(q==0){
             return head;
         }
-        while(1){
-            slow=slow->next;
-            fast=fast->next;
-            
-            if(fast->next==NULL){
-                break;
-            }
+        int r=n-q;
+        
+        ListNode* temp1=head;
+        int i=0;
+        while(i<(r-1)){
+            temp1=temp1->next;
+            i+=1;
+        }
+        ListNode * temp2=temp1->next;
+        temp1->next=NULL;
 
+        ListNode * head1=temp2;
+        while(temp2!=NULL){
+             if(temp2->next==NULL){
+                temp2->next=head;
+                return head1;
+             }
+             temp2=temp2->next;
         }
        
-        fast->next=curr;
-        head=slow->next;
-        slow->next=NULL;
-      
         return head;
     }
 };
