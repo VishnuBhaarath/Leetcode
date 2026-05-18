55fa2a3f15ebe0a38c2c2f77610b37ffee8c4980
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-18 12:59:25 +0530

    Time: 85 ms (62.97%) | Memory: 80.3 MB (49.42%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.cpp
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -12,15 +12,16 @@ public:
             for (int size = q.size(); size > 0; --size) {
                 int i = q.front(); q.pop();
                 if (i == n - 1) return step; // Reached to last index
-                vector<int>& next = indicesOfValue[arr[i]];
+                vector<int> &next = indicesOfValue[arr[i]];
                 next.push_back(i - 1); next.push_back(i + 1);
                 for (int j : next) {
                     if (j >= 0 && j < n && !visited[j]) {
+                        
                         visited[j] = true;
                         q.push(j);
                     }
                 }
-                next.clear(); // avoid later lookup indicesOfValue arr[i]
+               next.clear(); // avoid later lookup indicesOfValue arr[i]
             }
             step++;
         }
