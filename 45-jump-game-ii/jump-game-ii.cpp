91f583eaa0f91bf41228ed5ba26574653af6b8eb
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-18 10:48:27 +0530

    Time: 63 ms (18.92%) | Memory: 21.7 MB (14.68%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -5,18 +5,24 @@ public:
         vector<int> dp(n,-1);
         dp[0]=0;
         for(int i=0;i<nums.size();i++){
-              int m=i+nums[i]+1;
-              for(int j=i+1;j<min(m,n);j++){
-                 if(dp[j]==-1){
-                    dp[j]=1+dp[i];
+             if(dp[i]==-1){
+                return -1;
+             }
+             else{
+                 int r=i+nums[i];
+                 r=min(r,n-1);
+                 int j=i+1;
+                 while(j<=r){
+                     if(dp[j]==-1){
+                        dp[j]=1+dp[i];
+                     }
+                     else{
+                        dp[j]=min(dp[j],1+dp[i]);
+                     }
+                     j+=1;
                  }
-                 else{
-                    dp[j]=min(dp[j],1+dp[i]);
-                 }
-              }
+             }
         }
-
-       
         return dp[n-1];
     }
 };
