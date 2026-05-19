b9bd833c5eeab4d1e3bd5561a0f5058f21c1e1a4
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-19 10:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (46.01%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -4,48 +4,26 @@ public:
         int n=nums.size();
         int l=0;
         int r=n-1;
-    if(nums[0]<nums[r]){
-        return nums[0];
-    }
-    if(n==1){
-        return nums[0];
-    }
+        int st=nums[0];
+        if(nums[0]<nums[r]){
+            return nums[0];
+        }
+        if(n==1){
+            return nums[0];
+        }
+        int ans=5000;
         while(l<=r){
-            int m=l+(r-l)/2;
-            
-            if(m-1>=0 && m+1<n){
-                if(nums[m]<nums[m-1] && nums[m]<nums[m+1]){
-                    return nums[m];
-                }
-                if(nums[m]>nums[m-1] && nums[m]<nums[m+1]){
-                    if(nums[m]>nums[0]){
-                        l=m+1;
-                    }
-                    else{
-                        r=m-1;
-                    }
-                }
-                else if(nums[m]>nums[m-1] && nums[m]>nums[m+1]){
-                    return nums[m+1];
-                }
+            int mid=l+(r-l)/2;
+          
+            ans=min(ans,nums[mid]);
+         
+            if(nums[mid]<st){
+                r=mid-1;
             }
-            else if(m-1>=0){
-                if(nums[m]>nums[m-1]){
-                    r=m-1;
-                }
-                else {
-                    return nums[m];
-                }
-            }
-            else if(m+1<n){
-                if(nums[m+1]>nums[m]){
-                    return nums[m];
-                }
-                else{
-l=m+1;
-                }
+            else {
+                l=mid+1;
             }
         }
-        return -1;
+        return ans;
     }
 };
