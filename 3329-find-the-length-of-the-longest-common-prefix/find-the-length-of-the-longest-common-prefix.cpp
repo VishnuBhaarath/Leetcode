aa446597066c8ba789510e2a57cafe27b68749b6
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-21 10:36:17 +0530

    Time: 1300 ms (5.16%) | Memory: 156.3 MB (54.67%) - LeetSync

---
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        int ans=0;
+        set<string> us;
+        for(int i=0;i<arr1.size();i++){
+             string st1=to_string(arr1[i]);
+             string temp="";
+           
+             for(int j=0;j<st1.size();j++){
+                temp+=st1[j];
+                us.insert(temp);
+             }
+        }
+       
+      
+            for(int j=0;j<arr2.size();j++){
+              
+                string s2=to_string(arr2[j]);
+                int k=0;
+                int n=s2.size();
+                
+                int cnt=0;
+                string temp="";
+                while(k<n){
+                     temp+=s2[k];
+                     if(us.find(temp)!=us.end()){
+                        int sz=temp.size();
+                        ans=max(ans,sz);
+                     }
+                    
+                     k+=1;
+                }
+                ans=max(ans,cnt);
+            }
+        
+        return ans;
+    }
+};
