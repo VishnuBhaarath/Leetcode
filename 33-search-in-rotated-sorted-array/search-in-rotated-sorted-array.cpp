1ab957df9c4bd403b8074f439f024ccbc2d42d93
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-23 12:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.52%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,46 +1,62 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0;
-        int r = n - 1;
-        int st = nums[n - 1];
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            int left = mid - 1;
-            int right = mid + 1;
-            if (nums[mid] > st) {
-                if (target <= st) {
-                    l = mid + 1;
-                } else {
-                    if (target < nums[mid]) {
-                        r = mid - 1;
-                    } else if (target > nums[mid]) {
-                        l = mid + 1;
-                    } else if (target == nums[mid]) {
-                        return mid;
+        int n=nums.size();
+        int l=0;
+        int r=n-1;
+       if(target==nums[n-1]){
+        return n-1;
+       }
+       if(target==nums[0]){
+        return 0;
+       }
+        while(l<=r){
+            int m=l+(r-l)/2;
+          
+            if(nums[m]==target){
+                return m;
+            }
+            else if(nums[m]<=nums[0] && nums[m]<=nums[n-1]){
+               
+                if(target<nums[m]){
+                    r=m-1;
+                }
+                else{
+                    
+                    if(target<nums[n-1]){
+                        l=m+1;
+                    }
+                    else{
+                        r=m-1;
                     }
                 }
-            } else if (nums[mid] < st) {
-                if (target > st) {
-                    r = mid - 1;
-                } else {
-                    if (target > nums[mid]) {
-                        l = mid + 1;
-                    } else if (target < nums[mid]) {
-                        r = mid - 1;
-                    } else if (target == nums[mid]) {
-                        return mid;
+                
+            }
+            else if(nums[m]>=nums[0] && nums[m]>=nums[n-1]){
+                
+if(target>nums[m]){
+      
+                    l=m+1;
+                }
+                else{
+                    if(target>nums[0]){
+                        r=m-1;
+                    }
+                    else{
+                        l=m+1;
                     }
                 }
             }
-            else{
-                if(nums[mid]==target){
-                    return mid;
+            else if(nums[m]>=nums[0] && nums[m]<=nums[n-1]){
+                if(target>nums[m]){
+                    l=m+1;
                 }
-                return -1;
+                else{
+                    r=m-1;
+                }
+               
             }
-           
+          
         }
         return -1;
     }
