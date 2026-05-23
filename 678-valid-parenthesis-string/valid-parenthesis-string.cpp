58f7e71375fb2a21fa22787e2611b4204408abb8
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-23 14:18:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.47%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,26 +1,52 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int leftMin = 0, leftMax = 0;
+        stack<char> st1,st2;
 
-        for (char c : s) {
-            if (c == '(') {
-                leftMin++;
-                leftMax++;
-            } else if (c == ')') {
-                leftMin--;
-                leftMax--;
-            } else {
-                leftMin--;
-                leftMax++;
-            }
-            if (leftMax < 0) {
-                return false;
-            }
-            if (leftMin < 0) {
-                leftMin = 0;
-            }
+        for(int i=0;i<s.size();i++){
+             if(s[i]=='('){
+                st1.push(i);
+             }
+             else if(s[i]=='*'){
+                st2.push(i);
+             }
+             else{
+                if(st1.empty() && st2.empty()){
+                    return false;
+                }
+                else if(st1.size()!=0){
+                    st1.pop();
+                }
+                else{
+                    st2.pop();
+                }
+             }
         }
-        return leftMin == 0;
+        if(st1.size()==0){
+            return true;
+        }
+        if(st1.size()!=0 && st2.size()==0){
+            return false;
+        }
+        if(st1.top()>st2.top()){
+            return false;
+        }
+
+        while(!st1.empty()){
+             if(st1.top()<st2.top()){
+                st1.pop();
+                st2.pop();
+             }
+             else{
+                return false;
+             }
+             if(st1.empty()){
+                return true;
+             }
+             if(st2.empty()){
+                return false;
+             }
+        }
+        return true;
     }
 };
