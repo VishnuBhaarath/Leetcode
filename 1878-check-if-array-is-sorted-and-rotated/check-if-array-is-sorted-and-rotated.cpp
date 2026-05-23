c00cb33099f00f58ffc6b2178aeb27e02148c858
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-23 10:24:17 +0530

    Time: 3 ms (2.60%) | Memory: 12.2 MB (5.20%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -10,17 +10,34 @@ public:
         if(cnt==nums.size()){
             return true;
         }
-        for(int i=0;i<n;i++){
-             if(v1[i+1]>=v1[i]){
+        int j=0;
+        int i=0;
+        while(j<(2*nums.size())){
+             if(nums[i+1]>=nums[i]){
                 cnt+=1;
              }
              else{
                 cnt=1;
              }
+             i+=1;
+             j+=1;
+             if(cnt==nums.size()){
+                return true;
+             }
+             if(i==nums.size()-1){
+                if(nums[0]>=nums[i]){
+                    cnt+=1;
+                }
+                else{
+                    cnt=1;
+                }
+                i=0;
+             }
              if(cnt==nums.size()){
                 return true;
              }
         }
+        
         
         return false;
     }
