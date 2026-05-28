ac9c79817cb570cf919597174c5b285ae57a57d9
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-05-28 21:40:47 +0530

    Time: 1322 ms (5.01%) | Memory: 156.4 MB (38.83%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -2,41 +2,26 @@ class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         int ans=0;
-        set<string> us;
+        set<string> s;
         for(int i=0;i<arr1.size();i++){
-             string st1=to_string(arr1[i]);
-             string temp="";
-           
-             for(int j=0;j<st1.size();j++){
-                temp+=st1[j];
-                us.insert(temp);
-             }
-        }
-       
-      
-            for(int j=0;j<arr2.size();j++){
-              
-                string s2=to_string(arr2[j]);
-                int k=0;
-                int n=s2.size();
-                
-                int cnt=0;
-                string temp="";
-                while(k<n){
-                     temp+=s2[k];
-                     if(us.find(temp)!=us.end()){
-                        int sz=temp.size();
-                        ans=max(ans,sz);
-                     }
-                     else{
-                        break;
-                     }
-                    
-                     k+=1;
-                }
-                ans=max(ans,cnt);
+            string st=to_string(arr1[i]);
+            string temp="";
+            for(int j=0;j<st.size();j++){
+                temp+=st[j];
+                s.insert(temp);
             }
-        
+        }
+//int ans=0;
+        for(int j=0;j<arr2.size();j++){
+            string st=to_string(arr2[j]);
+            string temp="";
+            for(int j=0;j<st.size();j++){
+                temp+=st[j];
+                if(s.count(temp)!=0){
+                    ans=max(ans,(int)temp.size());
+                }
+            }
+        }
         return ans;
     }
 };
