50e584790d28deaffcfec115cc46a98bfe067cc6
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-06-02 11:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (82.71%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -2,38 +2,42 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> v(numCourses,0);
+        vector<vector<int>> adj(numCourses);
         vector<int> ans;
-        map<int,vector<int>> umap;
+       // set<int> s;
         for(int i=0;i<prerequisites.size();i++){
             v[prerequisites[i][0]]+=1;
-            umap[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-
         queue<int> q;
+        int cnt=0;
         for(int i=0;i<v.size();i++){
             if(v[i]==0){
                 q.push(i);
                 ans.push_back(i);
+                cnt+=1;
+              //  s.insert(i);
             }
         }
-        int cnt=q.size();
+      
         while(!q.empty()){
             int tp=q.front();
             q.pop();
-            vector<int> v1=umap[tp];
-            for(int j=0;j<v1.size();j++){
-                v[v1[j]]-=1;
-                if(v[v1[j]]==0){
-                    q.push(v1[j]);
-                    ans.push_back(v1[j]);
+            for(int i=0;i<adj[tp].size();i++){
+                v[adj[tp][i]]-=1;
+                if(v[adj[tp][i]]==0){
+            ans.push_back(adj[tp][i]);
+                        //s.insert(adj[tp][i]);
+                        q.push(adj[tp][i]);
+                    
                     cnt+=1;
                 }
             }
         }
-        if(cnt!=numCourses){
-            ans.clear();
-        }
-        
+if(cnt==numCourses){
+    return ans;
+}
+ans.clear();
         return ans;
     }
 };
