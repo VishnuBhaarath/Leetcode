5f729455dcd0c05f9814b56b4bd058515c6578f5
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-06-08 21:58:59 +0530

    Time: 15 ms (97.41%) | Memory: 14.4 MB (93.65%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        vector<vector<pair<int, int>>> adj(n);
-        for(int i=0;i<points.size();i++){
-            for(int j=i+1;j<points.size();j++){
-                int dist=abs(points[j][0]-points[i][0])+abs(points[j][1]-points[i][1]);
-               adj[i].push_back({dist, j});
-               adj[j].push_back({dist, i});
+         int n = points.size(), node = 0;
+        vector<int> dist(n, 100000000);
+        vector<bool> visit(n, false);
+        int edges = 0, res = 0;
 
+        while (edges < n - 1) {
+            visit[node] = true;
+            int nextNode = -1;
+            for (int i = 0; i < n; i++) {
+                if (visit[i]) continue;
+                int curDist = abs(points[i][0] - points[node][0]) +
+                               abs(points[i][1] - points[node][1]);
+                dist[i] = min(dist[i], curDist);
+                if (nextNode == -1 || dist[i] < dist[nextNode]) {
+                    nextNode = i;
+                }
             }
+            res += dist[nextNode];
+            node = nextNode;
+            edges++;
         }
-        //int n=points.size();
-        vector<int> v(n+1,0);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        int ans=0;
-        while(!pq.empty()){
-             pair<int,int> p=pq.top();
-             pq.pop();
-             int cost=p.first;
-             int idx=p.second;
-            
-             if(v[idx]==1){
-                continue;
-             }
-             v[idx]=1;
-             ans+=cost;
-             for(auto x:adj[idx]){
-                 pair<int,int> p1=x;
-                 
-                 if(v[p1.second]==0){
-                    pq.push({p1.first,p1.second});
-                 }
-               
-             }
-        }
-          
-        return ans;
-
-       
-
+        return res;
     }
 };
