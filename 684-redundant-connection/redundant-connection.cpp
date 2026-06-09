3fea8055e03e2658af538fda8de41ee14e77670a
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-06-09 21:54:16 +0530

    Time: 4 ms (25.85%) | Memory: 14.7 MB (18.84%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -4,50 +4,46 @@ public:
         int n=edges.size();
         vector<vector<int>> adj(n+1);
         vector<int> v(n+1,0);
+vector<int> v1(n+1,0);
         for(int i=0;i<edges.size();i++){
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
-            v[edges[i][1]]+=1;
             v[edges[i][0]]+=1;
+            v[edges[i][1]]+=1;
         }
-         queue<int> q;
-         vector<int> visited(n+1,0);
-         for(int i=0;i<v.size();i++){
+        queue<int> q;
+        
+        for(int i=1;i<v.size();i++){
             if(v[i]<=1){
-                q.push(i);
-                visited[i]=1;
+q.push(i);
+v1[i]+=1;
             }
         }
 
         while(!q.empty()){
-            int u=q.front();
-            v[u]-=1;
+            int tp=q.front();
             q.pop();
-            for(int i=0;i<adj[u].size();i++){
-                v[adj[u][i]]-=1;
-               if(v[adj[u][i]] <= 1 && visited[adj[u][i]] == 0) {
-                    q.push(adj[u][i]);
-                    visited[adj[u][i]]=1;
-                }
+            v[tp]-=1;
+            vector<int> v2=adj[tp];
+            for(int i=0;i<v2.size();i++){
+                 v[v2[i]]-=1;
+                 if(v[v2[i]]<=1 && v1[v2[i]]==0){
+                    v1[v2[i]]=1;
+                    q.push(v2[i]);
+                 }
+            }
 
+        }
+        vector<int> ans;
+        for(int i=edges.size()-1;i>=0;i--){
+            int x=edges[i][0];
+            int y=edges[i][1];
+            if(v[x]>1 && v[y]>1){
+                ans.push_back(x);
+                ans.push_back(y);
+                return ans;
             }
         }
-        
-       for(int i=0;i<v.size();i++){
-         if(v[i]>1){
-            cout<<i;
-            cout<<"\n";
-         }
-       }
-       for(int j=edges.size()-1;j>=0;j--){
-          int u=edges[j][0];
-          int w=edges[j][1];
-
-          if(v[u]>1 && v[w]>1){
-            return {u,w};
-          }
-       }
-return {};
+        return ans;
     }
-   
 };
