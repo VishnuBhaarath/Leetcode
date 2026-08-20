835a6efb0b152e34abd073d21f4eaf63380071b9
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-08-20 11:04:39 +0530

    Time: 81 ms (6.91%) | Memory: 152.2 MB (20.14%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,57 +1,38 @@
 class MinStack {
 public:
-    //stack<int> s;
-    //stack<int> s1;
-  //  unordered_map<int,int> umap;
-    stack<pair<int,int>> s;
-    stack<pair<int,int>> s1;
-   
-    int idx=0;
+    stack<int> s;
+    stack<int> s1;
     MinStack() {
         
     }
     
     void push(int value) {
-       
-       
+        s.push(value);
         if(s1.empty()){
-            s1.push({idx,value});
+            s1.push(value);
         }
         else{
-            int tp=s1.top().second;
-            if(tp>value){
-                s1.push({idx,value});
+            if(s1.top()<=value){
+                s1.push(s1.top());
+            }
+            else{
+                s1.push(value);
             }
         }
-         s.push({idx,value});
-         // umap[idx]=value;
-        idx+=1;
+        
     }
     
     void pop() {
-         int tp=s.top().first;;
-          s.pop();
-          if(s1.size()>0){
-        while(s1.top().first>=tp){
-            s1.pop();
-            if(s1.empty()){
-                break;
-            }
-        }}
-       
+        s.pop();
+        s1.pop();
     }
     
     int top() {
-        int tp=s.top().second;
-       
-        return tp;
+        return s.top();
     }
     
     int getMin() {
-        int tp=s.top().first;
-      
-       
-        return s1.top().second;
+        return s1.top();
     }
 };
 
