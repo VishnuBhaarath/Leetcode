809b2e0d000e243b25eb630cfd00d5281331623f
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-08-20 12:48:47 +0530

    Time: 11 ms (47.64%) | Memory: 11.3 MB (83.27%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,40 +1,52 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (t.empty()) return "";
+        unordered_map<char,int> umap;
 
-        unordered_map<char, int> countT, window;
-        for (char c : t) {
-            countT[c]++;
+        for(int i=0;i<t.size();i++){
+            umap[t[i]]+=1;
         }
+        int l=0;
+        int r=0;
+        int sz=-1;
+int cnt=0;
+        int st=0;
+        int n=s.size();
+        int m=t.size();
 
-        int have = 0, need = countT.size();
-        pair<int, int> res = {-1, -1};
-        int resLen = INT_MAX;
-        int l = 0;
-
-        for (int r = 0; r < s.length(); r++) {
-            char c = s[r];
-            window[c]++;
-
-            if (countT.count(c) && window[c] == countT[c]) {
-                have++;
-            }
-
-            while (have == need) {
-                if ((r - l + 1) < resLen) {
-                    resLen = r - l + 1;
-                    res = {l, r};
+        while(r<n){
+            
+             umap[s[r]]-=1;
+             if(umap[s[r]]>0){
+                cnt+=1;
+             }
+             if(umap[s[r]]==0){
+                cnt+=1;
+                while(cnt==m){
+                    if(sz==-1){
+                        sz=(r-l+1);
+                        st=l;
+                    }
+                    else{
+                        if(r-l+1 < sz){
+                            sz=r-l+1;
+                            st=l;
+                        }
+                    }
+                 
+                    umap[s[l]]+=1;
+                    if(umap[s[l]]>0){
+                        cnt-=1;
+                    }
+                    l+=1;
                 }
-
-                window[s[l]]--;
-                if (countT.count(s[l]) && window[s[l]] < countT[s[l]]) {
-                    have--;
-                }
-                l++;
-            }
+             }
+             r+=1;
         }
-
-        return resLen == INT_MAX ? "" : s.substr(res.first, resLen);
+     
+        if(sz==-1){
+            return "";
+        }
+        return s.substr(st,sz);
     }
 };
