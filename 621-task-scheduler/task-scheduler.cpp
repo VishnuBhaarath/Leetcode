ebddeb991496e51dcdd75efafaf925644c1fcd01
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-08-24 11:02:45 +0530

    Time: 2 ms (73.90%) | Memory: 38.3 MB (84.08%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,53 +1,20 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> v(26,0);
-        for(int i=0;i<tasks.size();i++){
-            v[tasks[i]-'A']+=1;
+          vector<int> count(26, 0);
+        for (char task : tasks) {
+            count[task - 'A']++;
         }
-        priority_queue<int> pq;
-        for(int i=0;i<v.size();i++){
-           
-            if(v[i]!=0)
-            pq.push(v[i]);
+
+        int maxf = *max_element(count.begin(), count.end());
+        int maxCount = 0;
+        for (int i : count) {
+            if (i == maxf) {
+                maxCount++;
+            }
         }
-        int cnt=0;
-        while(!pq.empty()){
-             vector<int> v1;
-             cnt+=1;
-             int tp=pq.top();
-             pq.pop();
-             tp-=1;
-             
-             int t=0;
-             if(tp!=0){
-                v1.push_back(tp);
-             }
-             else{
-                t=1;
-             }
-             for(int i=0;i<n;i++){
-                if(pq.empty()){
-                    if(t==1){
-                        break;
-                    }
-                    cnt+=(n-i);
-                    break;
-                }
-                int tp1=pq.top();
-                pq.pop();
-                tp1-=1;
-                if(tp1!=0){
-                    v1.push_back(tp1);
-                }
-                cnt+=1;
-                
-             }
-             
-             for(int i=0;i<v1.size();i++){
-                pq.push(v1[i]);
-             }
-        }
-        return cnt;
+
+        int time = (maxf - 1) * (n + 1) + maxCount;
+        return max((int)tasks.size(), time);
     }
 };
