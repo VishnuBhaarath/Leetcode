c9c6976c18bbe4162c639d769a8fe5ef29c11a7c
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-08-27 11:43:38 +0530

    Time: 2 ms (34.78%) | Memory: 10.3 MB (9.72%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,143 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n=s.size();
+
+        int l=0;
+        int r=0;
+
+        int cnt=0;
+        int len=-1;
+        string temp="";
+        while(r<n){
+            if(s[r]=='1'){
+                if(cnt<k){
+                    cnt+=1;
+                    if(cnt==k){
+                          string s1=s.substr(l,(r-l+1));
+                        
+                        if(len==-1){
+                            len=s1.size();
+                            temp=s.substr(l,(r-l+1));
+                        }
+                        else{
+                            if(s1.size() ==len){
+                                if(s.substr(l,(r-l+1))< temp){
+                                    temp=s.substr(l,(r-l+1));
+                                }
+
+                            }
+                            else if(s1.size() < len){
+                                len=s1.size();
+                                temp=s.substr(l,(r-l+1));
+                            }
+
+                        }
+                        
+                    }
+                }
+                else{
+                    while(s[l]=='0'){
+                        l+=1;
+                    }
+                    l+=1;
+                      string s1=s.substr(l,(r-l+1));
+                 
+                   
+                    if(len==-1){
+                            len=s1.size();
+                            temp=s.substr(l,(r-l+1));
+                        }
+                        else{
+                            if(s1.size() ==len){
+                                if(s.substr(l,(r-l+1))< temp){
+                                    temp=s.substr(l,(r-l+1));
+                                }
+                            }
+                            else if(s1.size() < len){
+                                len=s1.size();
+                                temp=s.substr(l,(r-l+1));
+                            }
+                        }
+                  
+
+                    
+                }
+            }
+            else{
+                if(cnt==k){
+                      string s1=s.substr(l,(r-l+1));
+                     
+                      
+                      if(len==-1){
+                            len=s1.size();
+                            temp=s.substr(l,(r-l+1));
+                        }
+                        else{
+                            if(s1.size() ==len){
+                                if(s.substr(l,(r-l+1))< temp){
+                                    temp=s.substr(l,(r-l+1));
+                                }
+                            }
+                            else if(s1.size() < len){
+                                len=s1.size();
+                                temp=s.substr(l,(r-l+1));
+                            }
+                        }
+                        
+                }
+            }
+            while(s[l]=='0'){
+                l+=1;
+            }
+            string s1=s.substr(l,(r-l+1));
+                   
+                    
+                          if(cnt==k){
+                           if(len==-1){
+                            len=s1.size();
+                            temp=s1;
+                        }
+                        else{
+                            if(s1.size() ==len){
+                                if(s1< temp){
+                                    temp=s1;
+                                }
+                            }
+                            else if(s1.size() < len){
+                                len=s1.size();
+                                temp=s1;
+                            }
+                        }}
+                        
+            r+=1;
+        }
+        while(cnt==k){
+            if(s[l]=='0'){
+                l+=1;
+                string s1=s.substr(l,(r-l+1));
+                 
+                  if(len==-1){
+                            len=s1.size();
+                            temp=s.substr(l,(r-l+1));
+                        }
+                        else{
+                            if(s1.size() ==len){
+                                if(s.substr(l,(r-l+1))< temp){
+                                    temp=s.substr(l,(r-l+1));
+                                }
+                            }
+                            else if(s1.size() < len){
+                                len=s1.size();
+                                temp=s.substr(l,(r-l+1));
+                            }
+                        }
+                        
+            }
+            else{
+                break;
+            }
+        }
+        return temp;
+    }
+};
