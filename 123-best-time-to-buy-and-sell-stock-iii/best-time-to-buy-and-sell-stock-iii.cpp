ba3154791fe3162b8f4e25302db0d6d84f9ec16a
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-04 12:35:39 +0530

    Time: 472 ms (12.78%) | Memory: 224 MB (23.43%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
-    int func(vector<int> prices, int buy, int capacity,int i, vector<vector<vector<int>>> dp){
-        if(capacity==0){
-            return 0;
-        }
-        if(i==prices.size()){
-            return 0;
-        }
-        if(dp[i][capacity][buy]!=-1){
-            return dp[i][capacity][buy];
-        }
- 
-       if(buy){
-         return  dp[i][capacity][buy]= max((-prices[i]+ func(prices,0,capacity,i+1,dp)),(0+func(prices,1,capacity,i+1,dp)));
-       }
-       else{
-        return dp[i][capacity][buy] =max((prices[i]+func(prices,1,capacity-1,i+1,dp)),(0+func(prices,0,capacity,i+1,dp)));
-       }
-
-    }
-    int maxProfit(vector<int>& prices) {
-         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (3,vector<int> (2,0)));
-      // int val=func(prices,1,2,0,dp);
-
-    
-       for(int i=n-1;i>=0;i--){
-        for(int j=1;j<3;j++){
-            for(int k=0;k<2;k++){
-               
-                 if(k){
-         dp[i][j][k]= max((-prices[i]+ dp[i+1][j][0]),(0+ dp[i+1][j][1]));
-       }
-       else{
-        dp[i][j][k] =max((prices[i]+dp[i+1][j-1][1]),(0+dp[i+1][j][0]));
-       }
-
-                 
-            }
-        }
-       }
-
+  vector<vector<vector<int>>> dp;
+  int func(int i, vector<int>& prices, int buy, int cap,int n){
+      if(i==n){
+        return 0;
+      }
+      if(cap==0){
+        return 0;
+      }
+      
+      if(dp[i][buy][cap]!=-1){
+        return dp[i][buy][cap];
+      }
+      if(buy){
+        return dp[i][buy][cap]=max(-prices[i]+func(i+1,prices,0,cap,n), func(i+1,prices,1,cap,n));
+      }
+      else{
+        return  dp[i][buy][cap]=max(prices[i]+func(i+1,prices,1,cap-1,n), func(i+1,prices,0,cap,n));
+      }
+  }
+   
+   int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
 
       
-      
-       return dp[0][2][1];
+       dp.resize(n,vector<vector<int>>(2,vector<int> (3,-1)));
+        
+
+        return func(0,prices,1,2,prices.size());
     }
 };
