a9af742e84ae509540fb74254bfb1c0476fba322
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-15 08:10:04 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (35.93%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int dp1_buy = 0, dp1_sell = 0;
-        int dp2_buy = 0;
-
-        for (int i = n - 1; i >= 0; --i) {
-            int dp_buy = max(dp1_sell - prices[i], dp1_buy);
-            int dp_sell = max(dp2_buy + prices[i], dp1_sell);
-            dp2_buy = dp1_buy;
-            dp1_buy = dp_buy;
-            dp1_sell = dp_sell;
+    vector<vector<int>> dp;
+    int func(vector<int>& prices,int i,int buy,int n){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i][buy]!=-1){
+            return dp[i][buy];
+        }
+        if(buy){
+            return dp[i][buy]=max(-prices[i]+func(prices,i+1,0,n), func(prices,i+1,1,n));
+        }
+        else{
+          return dp[i][buy]=max(+prices[i]+func(prices,i+2,1,n),func(prices,i+1,0,n));
         }
 
-        return dp1_buy;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        dp.resize(n,vector<int>(2,-1));
+        return func(prices,0,1,n);
     }
 };
