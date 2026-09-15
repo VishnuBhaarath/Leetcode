b0f7902a3d5cfcf3910b03a85bb10f816821ab9f
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-15 19:27:28 +0530

    Time: 27 ms (58.82%) | Memory: 27.6 MB (34.46%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -10,12 +10,12 @@ public:
             for(int j=0;j<text2.size();j++){
                  if(text1[i]==text2[j]){
                     dp[i+1][j+1]=1+dp[i][j];
-                    dp[i+1][j+1]=max(dp[i+1][j+1],dp[i][j+1]);
-                    dp[i+1][j+1]=max(dp[i+1][j+1],dp[i+1][j]);
+                  //  dp[i+1][j+1]=max(dp[i+1][j+1],dp[i][j+1]);
+                   // dp[i+1][j+1]=max(dp[i+1][j+1],dp[i+1][j]);
                  }
                  else{
                     dp[i+1][j+1]=max(dp[i+1][j],dp[i][j+1]);
-                    dp[i+1][j+1]=max(dp[i][j],dp[i+1][j+1]);
+                  //  dp[i+1][j+1]=max(dp[i][j],dp[i+1][j+1]);
                  }
                  ans=max(ans,dp[i+1][j+1]);
             }
