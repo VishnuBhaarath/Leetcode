f3c506e5c9660329ef19f0b11a969fd0b0fb6381
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-15 08:24:19 +0530

    Time: 31 ms (5.11%) | Memory: 16.2 MB (35.93%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -18,7 +18,24 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        dp.resize(n,vector<int>(2,-1));
-        return func(prices,0,1,n);
+        dp.resize(n+2,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+        if(buy==1){
+             dp[i][buy]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+        }
+        else{
+            dp[i][buy]=max(+prices[i]+dp[i+2][1],dp[i+1][0]);
+        }
+        }}
+
+        for(int i=0;i<dp.size();i++){
+            for(int j=0;j<dp[i].size();j++){
+                cout<<dp[i][j];
+                cout<<" ";
+            }
+            cout<<"\n";
+        }
+        return dp[0][1];
     }
 };
