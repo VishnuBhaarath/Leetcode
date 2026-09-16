5bd63260c57a0ea9f059336ceb151f23d2997226
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-16 09:07:08 +0530

    Time: 12 ms (51.28%) | Memory: 16.9 MB (21.34%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
-
-    int check(string word1,string word2,int i ,int j,vector<vector<int>> &dp){
-        if(i==word1.size() && j==word2.size()){
-            return 0;
-        }
-        if(i==word1.size() && j!=word2.size()){
+    vector<vector<int>> dp;
+    int func(string &word1, string &word2,int i, int j){
+        if(i==word1.size()){
             return word2.size()-j;
         }
-        if(i!=word1.size() && j==word2.size()){
+        if(j==word2.size()){
             return word1.size()-i;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(word1[i]== word2[j]){
-            return dp[i][j]=check(word1,word2,i+1,j+1,dp);
+        if(word1[i]==word2[j]){
+            return dp[i][j]=func(word1,word2,i+1,j+1);
         }
         else{
-            return dp[i][j]=min(1+check(word1,word2,i+1,j,dp),1+check(word1,word2,i,j+1,dp));
+            return dp[i][j]=1+min(func(word1,word2,i+1,j),func(word1,word2,i,j+1));
         }
-return 0;
     }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(word1[i]==word2[j]){
-                    dp[i+1][j+1]=1+dp[i][j];
-                    ans=max(ans,dp[i+1][j+1]);
-                }
-                else{
-                    dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j]);
-                     ans=max(ans,dp[i+1][j+1]);
-                }
-            }
-        }
-       
-        return word1.size()+word2.size()-ans-ans;
+        dp.resize(n,vector<int>(m,-1));
+        return func(word1,word2,0,0);
     }
 };
