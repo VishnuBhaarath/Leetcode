9420333fe9ad775ae36418c73581729d6541a60c
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-18 07:57:40 +0530

    Time: 2727 ms (5.00%) | Memory: 421.3 MB (5.01%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    int cnt=0;
+//int cnt=0;
     vector<vector<int>> dp;
-    int func(string s, string t,int i,int j){
-         
-         if(j==t.size()){
-            
-           // cnt+=1;
-            return 1;
-         }
-         if(i==s.size()){
-            return 0;
-         }
-         if(dp[i][j]!=-1){
-            return dp[i][j];
-         }
-         if(s[i]==t[j]){
-        return dp[i][j]=func(s,t,i+1,j+1)+func(s,t,i+1,j);}
-
-         return dp[i][j]=func(s,t,i+1,j);
-    }
-    int numDistinct(string s, string t) {
-         string st="";
-         int n=s.size();
-         int m=t.size();
-         dp.resize(n,vector<int>(m,-1));
-         int val= func(s,t,0,0);
+    int func(string s,int i,int j,string t){
         
-         return dp[0][0];
+        if(j==t.size()){
+            return 1;
+        }
+        if(i==s.size()){
+            return 0;
+        }
+       
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==t[j]){
+            return dp[i][j]=func(s,i+1,j+1,t)+ func(s,i+1,j,t);
+        }
+        else{
+            return dp[i][j]=func(s,i+1,j,t);
+        }
+     
+        
+    }
+
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        dp.resize(n,vector<int>(m,-1));
+        return func(s,0,0,t);
+       // return cnt;
     }
 };
