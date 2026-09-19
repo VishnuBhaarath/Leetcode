0f96083114f61ba1109111ff88ac198fd2bc8933
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-19 09:22:22 +0530

    Time: 18 ms (2.22%) | Memory: 11.1 MB (98.68%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,46 +1,56 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
-    
-
-        if(grid[n-1][m-1]==1){
-            return 0;
-        }
-        if(grid[0][0]==1){
-            return 0;
-        }
-        if(n==1){
-            if(m==1){
-                if(grid[0][0]==0){
-                    return 1;
+    int uniquePathsWithObstacles(vector<vector<int>>& dp) {
+        int n=dp.size();
+        int m=dp[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(dp[i][j]==1){
+                    dp[i][j]=-1;
                 }
             }
         }
-        dp[0][0]=1;
-        for(int i=1;i<n;i++){
-            if(dp[i-1][0]==1 && grid[i][0]==0){
-                dp[i][0]=1;
-            }
+        if(dp[0][0]==-1){
+            return 0;
         }
+        if(dp[n-1][m-1]==-1){
+            return 0;
+        }
+    dp[0][0]=1;
         for(int j=1;j<m;j++){
-           
-            if(dp[0][j-1]==1 && grid[0][j]==0){
-                dp[0][j]=1;
+            if(dp[0][j]!=-1 && dp[0][j-1]!=-1){
+                dp[0][j]+=dp[0][j-1];
+            }
+
+        }
+    
+        for(int i=1;i<n;i++){
+            if(dp[i][0]!=-1 && dp[i-1][0]!=-1){
+                dp[i][0]+=dp[i-1][0];
             }
         }
-       
+
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
-                if(grid[i][j]==0){
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                if(dp[i][j]!=-1){
+                if(dp[i-1][j]!=-1){
+                    dp[i][j]+=dp[i-1][j];
+                }
+                if(dp[i][j-1]!=-1){
+                    dp[i][j]+=dp[i][j-1];
+                }
                 }
             }
         }
 
-
+        for(int i=0;i<dp.size();i++){
+            for(int j=0;j<dp[0].size();j++){
+                cout<<dp[i][j];
+                cout<<" ";
+            }
+            cout<<"\n";
+        }
         return dp[n-1][m-1];
+
     }
 };
