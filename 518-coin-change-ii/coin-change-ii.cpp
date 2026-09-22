eccc90148d22616e4b958c36907d9ae8d1ef1d79
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-22 19:22:34 +0530

    Time: 61 ms (7.05%) | Memory: 36.8 MB (40.39%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        if(amount==0){
-            return 1;
+    int cnt=0;
+    vector<vector<int>> dp;
+    int func(int amount,vector<int>&coins,int i,int sum){
+        
+        if(sum==amount){
+           return 1;
         }
-        sort(coins.begin(),coins.end());
-        unsigned long long n=coins.size();
-        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(amount+1,0));
-        
-        for(long long int i=0;i<coins.size();i++){
-           for(long long int j=0;j<=amount;j++){
-                if(j<coins[i]){
-                    dp[i+1][j]=dp[i][j];
-                }
-                else if(j==coins[i]){
-                    dp[i+1][j]=dp[i][j]+1;
-                }
-                else{
-                    if(i==0){
-                       
-                        if(j==(unsigned long long int)(coins[i])){
-                            cout<<"in";
-                            dp[i+1][j]=1;
-                        }
-                        else{
- dp[i+1][j]=dp[i][j]+dp[i+1][j-coins[i]];
-                        }
-                       
-                    }
-                    else{
-                       
-                        dp[i+1][j] = dp[i][j] + dp[i+1][j - coins[i]];}
-                        
-                    }
-                
-                   
-                }
-           }
-        
-        
-        return dp[n][amount];
+       
+        if(i==coins.size()){
+            return 0;
+        }
+        if(sum>amount){
+            return 0;
+        }
+        if(dp[i][sum]!=-1){
+            return dp[i][sum];
+        }
+        return dp[i][sum]=func(amount,coins,i,sum+coins[i]) + func(amount,coins,i+1,sum);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        dp.resize(n,vector<int>(amount+1,-1));
+        return func(amount,coins,0,0);
+       // return cnt;
     }
 };
