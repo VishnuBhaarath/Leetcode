5ca6c08943a847910b33cc3a9b93b50df86daf1c
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-22 10:34:41 +0530

    Time: 76 ms (6.23%) | Memory: 89.7 MB (5.15%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,40 +1,62 @@
 class Solution {
 public:
+    vector<vector<long long int>> dp;
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,0);
-        for(int i=0;i<coins.size();i++){
-             if(coins[i]<=amount){
-                dp[coins[i]]=1;
-                if(coins[i]==amount){
-                    return 1;
-                }
-             }
-        }
+        int n = coins.size();
+        dp.resize(n + 1, vector<long long int>(amount + 2, 0));
         if(amount==0){
             return 0;
         }
-         for(int i=1;i<dp.size();i++){
-          
-             if(dp[i]!=0){
-                for(int j=0;j<coins.size();j++){
-                      if(((long long int)(i)+(long long int)(coins[j]))<=(long long int) amount){
-                         if(dp[i+coins[j]]==0){
-                             dp[i+coins[j]]=dp[i]+1;
-                         }
-                         else{
-                            dp[i+coins[j]]=min(dp[i]+1,dp[i+coins[j]]);
-                         }
-                      }
+        // int ans= func(coins,amount,0,0);
+        int ans = -1;
+        sort(coins.begin(), coins.end());
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= amount; j++) {
+                if (j == coins[i]) {
+                    dp[i][j] = 1;
+                } else if (j > coins[i]) {
+                    if (dp[i][j - coins[i]] > 0) {
+                        if (dp[i][j] == 0) {
+                            dp[i][j] = 1 + dp[i][j - coins[i]];
+                        } else {
+                            if(1+dp[i][j-coins[i]]<dp[i][j]){
+                                dp[i][j]=1 + dp[i][j - coins[i]];
+                            }
+                            
+                        }
+                    }
+                    if (dp[i + 1][j - coins[i]] > 0) {
+                        if (dp[i][j] == 0) {
+                            dp[i][j] = 1 + dp[i + 1][j - coins[i]];
+                        } else {
+                            if(dp[i + 1][j - coins[i]]+1<dp[i][j]){
+                            dp[i][j] = 1 + dp[i + 1][j - coins[i]];
+                            }
+                            
+                        }
+                    }
+                    if (dp[i + 1][j] != 0) {
+                        if(dp[i][j]==0){
+                            dp[i][j]=dp[i+1][j];
+                        }
+                        dp[i][j] = min(dp[i][j], dp[i + 1][j]);
+                    }
                 }
-             }
-            
-         }
-       
-
-       if(dp[amount]==0){
-        return -1;
-       }
-
-       return dp[amount];
+                if (j == amount) {
+                    if (dp[i][j] != 0) {
+                        if (ans == -1) {
+                            ans = dp[i][j];
+                        } else {
+                            if(dp[i][j]<ans){
+                                ans=dp[i][j];
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        
+        return ans;
     }
 };
