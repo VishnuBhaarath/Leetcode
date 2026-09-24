816912062a78a0b8dab7de2422741ec740614fa7
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-24 08:29:51 +0530

    Time: 4 ms (72.19%) | Memory: 12.3 MB (86.06%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -24,8 +24,22 @@ public:
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        dp.resize(n+1,-1);
+        dp.resize(n+1,0);
         
+        for(int i=n-1;i>=0;i--){
+            int val=0;
+            int ans=0;
+            for(int j=i;j<min(i+k,n);j++){
+           
+            val=max(val,arr[j]);
+            int sz=(j-i+1);
+            ans=max(ans,(val*sz)+dp[j+1]);
+           
+          
+        }
+        dp[i]=ans;
+        }
+        return dp[0];
       return func(arr,k,0,n);
      
         return 84;
