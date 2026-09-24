2ee15831970dd514956d8099d86949355198a1d4
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-24 21:28:18 +0530

    Time: 19 ms (80.97%) | Memory: 14.1 MB (9.46%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -27,7 +27,26 @@ public:
         }
         v.push_back(n);
         int m=v.size();
-        dp.resize(m+2,vector<int>(m+2,-1));
-        return func(v,1,m-2);
+        dp.resize(m+2,vector<int>(m+2,0));
+       // return func(v,1,m-2);
+
+        for(int i=m-2;i>=1;i--){
+            for(int j=1;j<=m-2;j++){
+               
+                if(j<i){
+                   dp[i][j]=0;
+                }
+                else{ int ans=INT_MAX;
+                for(int k=i;k<=j;k++){
+                    int val= v[j+1]-v[i-1]+dp[k+1][j]+dp[i][k-1];
+                    ans=min(ans,val);
+                }
+                dp[i][j]=ans;
+                }
+                
+            }
+        }
+
+        return dp[1][m-2];
     }
 };
