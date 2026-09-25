fe93e0f9d98b3e8874f1f77ecc8fdeccbc85c171
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-25 08:54:17 +0530

    Time: 34 ms (5.02%) | Memory: 52 MB (5.01%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
-vector<string> v;
-    void func(int n,int m, int open,string s){
-         if(n==0 && m==0){
-       
-            v.push_back(s);
-         
-            
-            return;
-         }
-         
-         if(n>0){
-             s+='(';
-             func(n-1,m,open+1,s);
-             s.pop_back();
-         }
-         if(m>0 && open>0){
-            s+=')';
-            func(n,m-1,open-1,s);
-            s.pop_back();
-         }
+    vector<string> ans;
+    bool isvalid(string &s){
+        stack<char> st;
+        for(int j=0;j<s.size();j++){
+            if(st.empty()){
+                st.push(s[j]);
+            }
+            else if(s[j]=='('){
+                st.push(s[j]);
+            }
+            else{
+                if(st.top()=='('){
+                    st.pop();
+                }
+                else{
+                    st.push(')');
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+    void func(int i,int n,int cnt1,int cnt2,string s){
+        if(cnt1==0 && cnt2==0){
+            if(isvalid(s)){
+                ans.push_back(s);
+            }
+           
+        }
+
+        if(cnt1>0){
+            func(i+1,n,cnt1-1,cnt2,s+'(');
+        }
+        if(cnt2>0){
+            func(i+1,n,cnt1,cnt2-1,s+')');
+        }
     }
     vector<string> generateParenthesis(int n) {
-        string s="";
-        func(n,n,0,s);
-        
-        return v;
+        int i=0;
+        func(i,2*n,n,n,"");
+        return ans;
     }
 };
