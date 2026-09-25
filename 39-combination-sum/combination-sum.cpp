b2ebc783cffed1b536f1ffaa57152cf0f3c745a2
VishnuBhaarath <45665795+VishnuBhaarath@users.noreply.github.com>
2026-09-25 09:10:57 +0530

    Time: 80 ms (7.31%) | Memory: 72.3 MB (5.44%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-vector<vector<int>> res;
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        dfs(0, {}, 0, nums, target);
-        return res;
-    }
-      void dfs(int i, vector<int> cur, int total, vector<int>& nums, int target) {
-        if (total == target) {
-            res.push_back(cur);
+    vector<vector<int>> dp;
+    void func(vector<int>& candidates, int target,int sum,vector<int> v,int i){
+      
+        if(sum==target){
+            dp.push_back(v);
             return;
         }
-
-        for (int j = i; j < nums.size(); j++) {
-            if (total + nums[j] > target) {
-                return;
-            }
-            cur.push_back(nums[j]);
-            dfs(j, cur, total + nums[j], nums, target);
-            cur.pop_back();
+        if(i==candidates.size()){
+            return;
         }
+        if(sum>target){
+            return;
+        }
+           v.push_back(candidates[i]);
+           func(candidates,target,sum+candidates[i],v,i);
+           v.pop_back();
+           func(candidates,target,sum,v,i+1);
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        func(candidates,target,0,{},0);
+
+        return dp;
+        
     }
 };
